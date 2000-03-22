2620c402bc51338b92767935624ffe2cbe586260
mac <mac>
2000-03-22 14:30:57 +0000

    no message

--- eo/src/eoPersistent.cpp
+++ eo/src/eoPersistent.cpp
@@ -8,10 +8,3 @@ istream & operator >> ( istream& _is, eoPersistent& _o ) {
   _o.readFrom(_is);
   return _is;
 };
-
-// temporary location for the one and only eoRng object
-
-#include "eoRNG.h"
-
-eoRng rng;
-
