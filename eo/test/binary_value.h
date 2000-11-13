5682f296cd478b81783be8f58393ff21a5293d38
gustavoromero <gustavoromero>
2000-11-13 12:12:14 +0000

    fixing template type

--- eo/test/binary_value.h
+++ eo/test/binary_value.h
@@ -2,14 +2,12 @@
 
 //-----------------------------------------------------------------------------
 
-typedef eoBin<float> Chrom;
-
 /** Just a simple function that takes binary value of a chromosome and sets
     the fitnes.
     @param _chrom A binary chromosome 
 */
 
-float binary_value(const Chrom& _chrom)
+template <class Chrom> float binary_value(const Chrom& _chrom)
 {
   float sum = 0;
   for (unsigned i = 0; i < _chrom.size(); i++)
