65b1e9d2b6e0cf1078b4c399046c4d77d813b7a0
maartenkeijzer <maartenkeijzer>
2001-03-23 08:53:48 +0000

    added

---
+++ eo/doc/index.h
@@ -0,0 +1,15 @@
+
+/*! \mainpage Welcome to Evolving Objects
+
+  \section intro Introduction
+
+This index page is under construction. Who wants to fill it in?
+
+  \section install Installation
+
+Installing the package. 
+
+  \section design Overall Design 
+
+
+ */
