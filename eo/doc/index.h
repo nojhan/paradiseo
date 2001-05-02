b52ea27e2fd42ce93f813f0710ef4d349e8037c8
evomarc <evomarc>
2001-05-02 11:13:40 +0000

    Updated main pages of doc and tutorial. Doc now points to tutorial

--- eo/doc/index.h
+++ eo/doc/index.h
@@ -5,11 +5,14 @@
 
 This index page is under construction. Who wants to fill it in?
 
+  \section tutorial Tutorial
+
+However, you can already find some useful information in the <a href="../../tutorial/html/eoTutorial.html">tutorial</a>
+
   \section install Installation
 
 Installing the package. 
 
   \section design Overall Design 
-
 
  */
