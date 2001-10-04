f3db65795bc5f4a0a4b5eec5214315ee39d5630e
evomarc <evomarc>
2001-10-04 20:12:19 +0000

    Big modifications - now the init and most important the operators are handled in separate files make_genotype_xxx and make_op_xxx as it was done in the examples of Lesson4

--- eo/tutorial/Templates/create.sh
+++ eo/tutorial/Templates/create.sh
@@ -3,11 +3,17 @@ if ($#argv < 2) then
     echo Usage $argv[0] ApplicationName TargetDir
     exit
 endif
-sed s/eoMyStruct/eo$1/g eoMyStruct.tmpl > $2/eo$1.h
-sed s/eoMyStruct/eo$1/g init.tmpl > $2/eo$1Init.h
-sed s/eoMyStruct/eo$1/g evalFunc.tmpl > $2/eo$1EvalFunc.h
-sed s/eoMyStruct/eo$1/g mutation.tmpl > $2/eo$1Mutation.h
-sed s/eoMyStruct/eo$1/g quadCrossover.tmpl > $2/eo$1QuadCrossover.h
-sed s/eoMyStruct/eo$1/g eoMyStructEA.cpp > $2/eo$1EA.cpp
-sed s/eoMyStruct/eo$1/g Makefile.tmpl > $2/Makefile
+if (! -e $2) then
+    mkdir $2
+endif
+sed s/MyStruct/$1/g eoMyStruct.tmpl > $2/eo$1.h
+sed s/MyStruct/$1/g init.tmpl > $2/eo$1Init.h
+sed s/MyStruct/$1/g evalFunc.tmpl > $2/eo$1EvalFunc.h
+sed s/MyStruct/$1/g mutation.tmpl > $2/eo$1Mutation.h
+sed s/MyStruct/$1/g quadCrossover.tmpl > $2/eo$1QuadCrossover.h
+sed s/MyStruct/$1/g MyStructEA.cpp > $2/$1EA.cpp
+sed s/MyStruct/$1/g make_genotype_MyStruct.h > $2/make_genotype_$1.h
+sed s/MyStruct/$1/g make_op_MyStruct.h > $2/make_op_$1.h
+sed s/MyStruct/$1/g Makefile.tmpl > $2/Makefile
+
 
