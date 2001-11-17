8538cc453238b5d90caa049283c323decd84e100
evomarc <evomarc>
2001-11-17 07:55:39 +0000

    Bug in create_dsp when no additional library

--- eo/win/create_dsp.sh
+++ eo/win/create_dsp.sh
@@ -11,8 +11,14 @@ endif
 echo "Creating $2.dsp"
 echo s/DIRNAME/$2/g  > toto.sed
 echo s/SOURCENAME/$1/g >> toto.sed
+# additional libraries: ADDLIBR = name of "Release" library
+#                       ADDLIBD = name of "Debug" library
 if ($#argv == 3) then
-    echo s/ADDLIB/$3/g >> toto.sed
+    echo s/ADDLIBR/$3.lib/g >> toto.sed
+    echo s/ADDLIBD/$3d.lib/g >> toto.sed
+else   # remove the ADDLIBs !!!!
+    echo s/ADDLIBR//g >> toto.sed
+    echo s/ADDLIBD//g >> toto.sed
 endif
 
 sed -f toto.sed test_dsp.tmpl > $2.dsp
