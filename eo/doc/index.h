e1319605e69b120584ef7dc5b71ccbaada290192
evomarc <evomarc>
2002-03-15 06:41:54 +0000

    Added the lonk to SourceForge in the title, and to README in the "install" section

--- eo/doc/index.h
+++ eo/doc/index.h
@@ -1,5 +1,5 @@
 
-/*! \mainpage Welcome to Evolving Objects
+/*! \mainpage Welcome to <a href="http://eodev.sourceforge.net/">Evolving Objects</a>
 
   \section intro Introduction
 
@@ -11,7 +11,7 @@ However, you can already find some useful information in the <a href="../../tuto
 
   \section install Installation
 
-Installing the package. 
+Installing the package: everything is detailed in the <a href="../../README">README file in the main dir</a>
 
   \section design Overall Design 
 
