cf40eab455a1d32c183ce0650fa7b8ea87c7f53a
evomarc <evomarc>
2002-12-09 06:17:25 +0000

    Corrected a typo

--- eo/src/utils/eoParser.cpp
+++ eo/src/utils/eoParser.cpp
@@ -66,7 +66,7 @@ eoParser::eoParser ( unsigned _argc, char **_argv , string _programDescription, 
     programName( _argv[0]),
     programDescription( _programDescription), 
     needHelp(false, "help", "Prints this message", 'h'),
-    stopOnUnknownParam(true, "stopOnUnknownParam", "Stop if unkonwn param entered", '\0')
+    stopOnUnknownParam(true, "stopOnUnknownParam", "Stop if unkown param entered", '\0')
 {
     // need to process the param file first 
     // if we want command-line to have highest priority
