89a99dab17def7fdd612d7ac69981ffd05933ef2
okoenig <okoenig>
2003-02-27 19:18:07 +0000

    removed "using namespace std" statements from header files in EO -- "std::" identifier were added where necessary.

--- eo/tutorial/ParadisEO/Lesson3/binary_value.h
+++ eo/tutorial/ParadisEO/Lesson3/binary_value.h
@@ -7,7 +7,7 @@
     @param _chrom A binary chromosome 
 */
 // INIT
-double binary_value(const vector<bool>& _chrom)
+double binary_value(const std::vector<bool>& _chrom)
 {
   double sum = 0;
   for (unsigned i = 0; i < _chrom.size(); i++)
