1b59c07cba35a6b6db0e46a0657ac17786a9c8c1
kuepper <kuepper>
2005-10-01 11:59:14 +0000

    update docs clean Tutorial bump version-number for cvs

--- eo/acinclude.m4
+++ eo/acinclude.m4
@@ -39,35 +39,6 @@ AC_DEFUN([AC_TUTORIAL],[dnl
 
 
 dnl Available from the GNU Autoconf Macro Archive at:
-dnl http://www.gnu.org/software/ac-archive/htmldoc/ac_cxx_have_sstream.html
-dnl
-AC_DEFUN([AC_CXX_HAVE_SSTREAM],
-[AC_CACHE_CHECK(whether the compiler has stringstream,
-ac_cv_cxx_have_sstream,
-[AC_REQUIRE([AC_CXX_NAMESPACES])
- AC_LANG_SAVE
- AC_LANG_CPLUSPLUS
- AC_TRY_COMPILE([#include <sstream>
-#ifdef HAVE_NAMESPACES
-using namespace std;
-#endif
-#if __GNUC__ == 2 
-#if __GNUC_MINOR__ == 96
-#error("force_error_for_2_96")
-#endif
-#endif],[stringstream message; message << "Hello"; return 0;
-],
- ac_cv_cxx_have_sstream=yes, ac_cv_cxx_have_sstream=no)
- AC_LANG_RESTORE
-])
-if test "$ac_cv_cxx_have_sstream" = yes; then
-  AC_DEFINE(HAVE_SSTREAM,,[define if the compiler has stringstream])
-fi
-])
-
-
-
-dnl Available from the GNU Autoconf Macro Archive at:
 dnl http://www.gnu.org/software/ac-archive/htmldoc/ac_cxx_namespaces.html
 dnl
 AC_DEFUN([AC_CXX_NAMESPACES],
--- eo/doc/index.h
+++ eo/doc/index.h
@@ -1,18 +1,65 @@
+/** @mainpage Welcome to Evolving Objects
 
-/*! \mainpage Welcome to <a href="http://eodev.sourceforge.net/">Evolving Objects</a>
+@section intro Introduction
 
-  \section intro Introduction
+EO is a templates-based, ANSI-C++ compliant evolutionary computation library. It
+contains classes for almost any kind of evolutionary computation you might come
+up to - at least for the ones we could think of. It is component-based, so that
+if you don't find the class you need in it, it is very easy to subclass existing
+abstract or concrete classes.
 
-This index page is under construction. Who wants to fill it in?
 
-  \section tutorial Tutorial
 
-However, you can already find some useful information in the <a href="../../tutorial/html/eoTutorial.html">tutorial</a>
+@section tutorial Tutorial
 
-  \section install Installation
+The best place to learn about the features and approaches of EO is to look at
+the <a href="../../tutorial/html/eoTutorial.html">tutorial</a>.
 
-Installing the package: everything is detailed in the <a href="../../README">README file in the main dir</a>
 
-  \section design Overall Design 
 
- */
+@section install Installation
+
+Installing the package: everything is detailed in the <a
+href="README.html">README</a> file in the top-directory of the source-tree.
+
+
+
+@section design Overall Design
+
+For an introduction to the design of EO you can look at the <a
+href="http://eodev.sourceforge.net/eo/doc/LeCreusot.pdf">slides</a> from a talk
+at EA 2001 or the corresponding <a
+href="http://citeseer.csail.mit.edu/keijzer01evolving.html">article</a> in <a
+href="http://portal.acm.org/citation.cfm?id=727742"><em>Lecture Notes In
+Computer Science</em> <b>2310</b>, <em>Selected Papers from the 5th European
+Conference on Artificial Evolution</em></a>.
+*/
+
+
+
+/** @page Related Pages
+
+- SourceForge <a href="http://eodev.sourceforge.net">project page</a>
+- <a href="../../README">README</a>
+- <a href="../../NEWS">NEWS</a>
+*/
+
+
+
+/** @page Related Projects
+
+<h1>ParadisEO</h1>
+
+<a href="http://www.lifl.fr/~cahon/paradisEO">ParadisEO</a> is a project to
+extend EO to make use of dedicated parallel or distributed computing
+environments by <a href="http://www.lifl.fr/~cahon">Sébastien CAHON</a>.
+*/
+
+
+
+// Local Variables:
+// coding: iso-8859-1
+// mode: C++
+// c-file-style: "Stroustrup"
+// fill-column: 80
+// End:
