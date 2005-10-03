03b1689de99510eb5c8c4ab9a2b550c0d249d9ba
kuepper <kuepper>
2005-10-03 09:47:06 +0000

    Initialize booleans to get rid of compiler-warnings.

--- eo/tutorial/Lesson5/eoOneMaxMutation.h
+++ eo/tutorial/Lesson5/eoOneMaxMutation.h
@@ -25,43 +25,43 @@ template<class GenotypeT>
 class eoOneMaxMutation: public eoMonOp<GenotypeT>
 {
 public:
-  /**
-   * Ctor - no requirement
-   */
-// START eventually add or modify the anyVariable argument
-  eoOneMaxMutation()
-  //  eoOneMaxMutation( varType  _anyVariable) : anyVariable(_anyVariable)
-// END eventually add or modify the anyVariable argument
-  {
-    // START Code of Ctor of an eoOneMaxEvalFunc object
-    // END   Code of Ctor of an eoOneMaxEvalFunc object
-  }
+    /**
+     * Ctor - no requirement
+     */
+    // START eventually add or modify the anyVariable argument
+    eoOneMaxMutation()
+    //  eoOneMaxMutation( varType  _anyVariable) : anyVariable(_anyVariable)
+    // END eventually add or modify the anyVariable argument
+        {
+            // START Code of Ctor of an eoOneMaxEvalFunc object
+            // END   Code of Ctor of an eoOneMaxEvalFunc object
+        }
 
-  /// The class name. Used to display statistics
-  string className() const { return "eoOneMaxMutation"; }
+    /// The class name. Used to display statistics
+    string className() const { return "eoOneMaxMutation"; }
 
-  /**
-   * modifies the parent
-   * @param _genotype The parent genotype (will be modified)
-   */
-  bool operator()(GenotypeT & _genotype)
-  {
-    bool isModified;
-    // START code for mutation of the _genotype object
+    /**
+     * modifies the parent
+     * @param _genotype The parent genotype (will be modified)
+     */
+    bool operator()(GenotypeT & _genotype)
+        {
+            bool isModified(true);
+            // START code for mutation of the _genotype object
 
-       /** Requirement
-	* if (_genotype has been modified)
-	*     isModified = true;
-	* else
-	*     isModified = false;
-	*/
-    return isModified;
-    // END code for mutation of the _genotype object
-  }
+            /** Requirement
+             * if (_genotype has been modified)
+             *     isModified = true;
+             * else
+             *     isModified = false;
+             */
+            return isModified;
+            // END code for mutation of the _genotype object
+        }
 
 private:
 // START Private data of an eoOneMaxMutation object
-  //  varType anyVariable;		   // for example ...
+    //  varType anyVariable;		   // for example ...
 // END   Private data of an eoOneMaxMutation object
 };
 
