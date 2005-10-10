e9324fdb070574119876b1c86569302a6de0f2e5
kuepper <kuepper>
2005-10-10 21:13:27 +0000

    cleanup

--- eo/src/es/eoEsSimple.h
+++ eo/src/es/eoEsSimple.h
@@ -1,28 +1,24 @@
-// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+/* (c) GeNeura Team, 2000 - EEAAX 1999, Maarten Keijzer 2000
 
-//-----------------------------------------------------------------------------
-// eoEsSimple.h
-// (c) GeNeura Team, 2000 - EEAAX 1999, Maarten Keijzer 2000
-/* 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
+later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+This library is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU Lesser General Public License along
+with this library; if not, write to the Free Software Foundation, Inc., 59
+Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-    Contact: todos@geneura.ugr.es, http://geneura.ugr.es
-             Marc.Schoenauer@polytechnique.fr
-             mak@dhi.dk
+Contact: http://eodev.sourceforge.net
+         todos@geneura.ugr.es, http://geneura.ugr.es
+         Marc.Schoenauer@polytechnique.fr
+         mak@dhi.dk
  */
-//-----------------------------------------------------------------------------
+
 
 #ifndef _eoEsSimple_h
 #define _eoEsSimple_h
@@ -32,17 +28,16 @@
 
 #include <eoVector.h>
 
-/**
-\ingroup EvolutionStrategies
+/** Simple Evolution Strategy
 
-  One of the more simple evolution strategies, sporting just a single
-stdeviation for the entire chromosome. For more advanced versions
-see also eoEsStdev eoEsFull
+@ingroup EvolutionStrategies
 
-  @see eoEsStdev eoEsFull
+One of the more simple evolution strategies, sporting just a single stdeviation
+for the entire chromosome. For more advanced versions see also eoEsStdev
+eoEsFull
 
+@see eoEsStdev eoEsFull
 */
-
 template <class Fit>
 class eoEsSimple : public eoVector<Fit, double>
 {
@@ -50,21 +45,19 @@ public :
 
     typedef double Type;
 
-    eoEsSimple(void) : eoVector<Fit, double>() {}
+    eoEsSimple() : eoVector<Fit, double>() {}
 
-    virtual std::string className(void) const { return "eoEsSimple"; }
+    virtual std::string className() const { return "eoEsSimple"; }
 
     void printOn(std::ostream& os) const
     {
         eoVector<Fit,double>::printOn(os);
-
         os << ' ' << stdev << ' ';
     }
 
     void readFrom(std::istream& is)
     {
         eoVector<Fit,double>::readFrom(is);
-
         is >> stdev;
     }
 
@@ -72,3 +65,13 @@ public :
 };
 
 #endif
+
+
+
+// Local Variables:
+// coding: iso-8859-1
+// mode:C++
+// c-file-style: "Stroustrup"
+// comment-column: 35
+// fill-column: 80
+// End:
--- eo/src/es/eoEsStdev.h
+++ eo/src/es/eoEsStdev.h
@@ -1,47 +1,44 @@
-// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+/* (c) GeNeura Team, 2000 - EEAAX 1999 - Maarten Keijzer 2000
 
-//-----------------------------------------------------------------------------
-// eoEsStdev.h
-// (c) GeNeura Team, 2000 - EEAAX 1999 - Maarten Keijzer 2000
-/*
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
+later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+This library is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU Lesser General Public License along
+with this library; if not, write to the Free Software Foundation, Inc., 59
+Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-    Contact: todos@geneura.ugr.es, http://geneura.ugr.es
-             Marc.Schoenauer@polytechnique.fr
-             mak@dhi.dk
+Contact: http://eodev.sourceforge.net
+         todos@geneura.ugr.es, http://geneura.ugr.es
+         Marc.Schoenauer@polytechnique.fr
+         mak@dhi.dk
  */
-//-----------------------------------------------------------------------------
+
 
 #ifndef _eoEsStdev_h
 #define _eoEsStdev_h
 
 #include <eoVector.h>
 
-/**
-\ingroup EvolutionStrategies
 
-Evolutionary strategie style representation, supporting co-evolving standard
-deviations.
+/** Evolutionary Strategy with a standard deviation per parameter
+
+@ingroup EvolutionStrategies
+
+Evolutionary strategie style representation, supporting co-evolving
+standard deviations.
 */
-
 template <class Fit>
 class eoEsStdev : public eoVector<Fit, double>
 {
 public:
 
-    using eoEsStdev< Fit >::size;
+    using eoEsStdev<Fit>::size;
 
     typedef double Type;
 
@@ -71,3 +68,13 @@ public:
 };
 
 #endif
+
+
+
+// Local Variables:
+// coding: iso-8859-1
+// mode:C++
+// c-file-style: "Stroustrup"
+// comment-column: 35
+// fill-column: 80
+// End:
