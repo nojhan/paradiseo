59f3f6a56f4976aef929d6ec8e1a04cb860072c0
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2006-12-19 08:31:07 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@74 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-mo/autogen.sh
+++ trunk/paradiseo-mo/autogen.sh
@@ -55,6 +55,12 @@ grep "^AM_GNOME_GETTEXT" $srcdir/configure.in >/dev/null && {
   NO_AUTOMAKE=yes
 }
 
+(doxygen --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**WARNING**: You need to have \`doxygen' to generate the documentation."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+}
 
 # if no automake, don't bother testing for aclocal
 test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
--- trunk/paradiseo-mo/docs/html/search.php
+++ trunk/paradiseo-mo/docs/html/search.php
@@ -374,7 +374,7 @@ main();
 
 
 ?>
-<hr size="1"><address style="align: right;"><small>Generated on Tue Dec 12 14:28:37 2006 for PARADISEO-MO by&nbsp;
+<hr size="1"><address style="align: right;"><small>Generated on Mon Dec 18 15:12:36 2006 for PARADISEO-MO by&nbsp;
 <a href="http://www.doxygen.org/index.html">
 <img src="doxygen.png" alt="doxygen" align="middle" border="0"></a> 1.5.1 </small></address>
 </body>
