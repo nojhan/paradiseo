81871bb53696a42dd4832df022fc5efbf6ab7c17
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2006-12-19 08:41:33 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@88 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ trunk/paradiseo-mo/tutorial/examples/tsp/src/route_eval.h
@@ -0,0 +1,29 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+// "route_eval.h"
+
+// (c) OPAC Team, LIFL, 2003-2006
+
+/* LICENCE TEXT
+   
+   Contact: paradiseo-help@lists.gforge.inria.fr
+*/
+
+#ifndef route_eval_h
+#define route_eval_h
+
+#include <eoEvalFunc.h>
+
+#include "route.h"
+
+/** Route Evaluator */
+class RouteEval : public eoEvalFunc <Route> {
+  
+public :
+    
+  void operator () (Route & __route) ;
+  
+} ;
+
+
+#endif
---
+++ trunk/paradiseo-mo/tutorial/examples/tsp/src/two_opt_tabu_list.h
@@ -0,0 +1,38 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+// "two_opt_tabu_list.h"
+
+// (c) OPAC Team, LIFL, 2003-2006
+
+/* LICENCE TEXT
+   
+   Contact: paradiseo-help@lists.gforge.inria.fr
+*/
+
+#ifndef two_opt_tabu_list_h
+#define two_opt_tabu_list_h
+
+#include <moTabuList.h>
+#include "two_opt.h"
+#include "route.h"
+
+/** The table of tabu movements, i.e. forbidden edges */
+class TwoOptTabuList : public moTabuList <TwoOpt> {
+  
+public :
+  
+  bool operator () (const TwoOpt & __move, const Route & __sol) ;
+  
+  void add (const TwoOpt & __move, const Route & __sol) ;
+  
+  void update () ;
+
+  void init () ;
+  
+private :
+  
+  std :: vector <std :: vector <unsigned> > tabu_span ;
+  
+} ;
+
+#endif
