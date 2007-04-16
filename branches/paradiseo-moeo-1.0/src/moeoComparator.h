48480f03f98b8ea44505962aded5659c2bea9a3d
liefooga <liefooga@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-04-16 08:22:49 +0000

    update doc
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@237 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/paradiseo-moeo-1.0/src/moeoComparator.h
+++ branches/paradiseo-moeo-1.0/src/moeoComparator.h
@@ -4,7 +4,7 @@
 // moeoComparator.h
 // (c) OPAC Team (LIFL), Dolphin Project (INRIA), 2007
 /*
-    This library...
+    This library..
 
     Contact: paradiseo-help@lists.gforge.inria.fr, http://paradiseo.gforge.inria.fr
  */
