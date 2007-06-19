c659538bca64d2c5d65b542845904920eac7271a
liefooga <liefooga@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-06-19 11:58:53 +0000

    update doc
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@338 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/paradiseo-moeo-1.0/src/moeoNSGAII.h
+++ branches/paradiseo-moeo-1.0/src/moeoNSGAII.h
@@ -27,10 +27,10 @@
 #include <moeoFastNonDominatedSortingFitnessAssignment.h>
 
 /**
- * The NSGA-II algorithm as described in:
+ * NSGA-II (Non-dominated Sorting Genetic Algorithm II) as described in:
  * Deb, K., S. Agrawal, A. Pratap, and T. Meyarivan : "A fast elitist non-dominated sorting genetic algorithm for multi-objective optimization: NSGA-II".
  * In IEEE Transactions on Evolutionary Computation, Vol. 6, No 2, pp 182-197 (April 2002).
- * This class builds the NSGA-II algorithm only by using the components of the ParadisEO-MOEO framework.
+ * This class builds the NSGA-II algorithm only by using the fine-grained components of the ParadisEO-MOEO framework.
  */
 template < class MOEOT >
 class moeoNSGAII: public moeoEA < MOEOT >
@@ -42,7 +42,7 @@ public:
      * @param _maxGen number of generations before stopping
      * @param _eval evaluation function
      * @param _op variation operator
-    */
+     */
     moeoNSGAII (unsigned _maxGen, eoEvalFunc < MOEOT > & _eval, eoGenOp < MOEOT > & _op) :
             defaultGenContinuator(_maxGen), continuator(defaultGenContinuator), popEval(_eval), select(2),
             replace(fitnessAssignment, diversityAssignment), genBreed(select, _op), breed(genBreed)
@@ -54,7 +54,7 @@ public:
      * @param _maxGen number of generations before stopping
      * @param _eval evaluation function
      * @param _op variation operator
-    */
+     */
     moeoNSGAII (unsigned _maxGen, eoEvalFunc < MOEOT > & _eval, eoTransform < MOEOT > & _op) :
             defaultGenContinuator(_maxGen), continuator(defaultGenContinuator), popEval(_eval), select(2),
             replace(fitnessAssignment, diversityAssignment), genBreed(select, _op), breed(genBreed)
@@ -62,13 +62,13 @@ public:
 
 
     /**
-     * Ctor with crossover, mutation and corresponding rates.
+     * Ctor with a crossover, a mutation and their corresponding rates.
      * @param _maxGen number of generations before stopping
-    * @param _eval evaluation function
-    * @param _crossover crossover
-    * @param _pCross crossover probability
-    * @param _mutation mutation
-    * @param _pMut mutation probability
+     * @param _eval evaluation function
+     * @param _crossover crossover
+     * @param _pCross crossover probability
+     * @param _mutation mutation
+     * @param _pMut mutation probability
      */
     moeoNSGAII (unsigned _maxGen, eoEvalFunc < MOEOT > & _eval, eoQuadOp < MOEOT > & _crossover, double _pCross, eoMonOp < MOEOT > & _mutation, double _pMut) :
             defaultGenContinuator(_maxGen), continuator(defaultGenContinuator), popEval(_eval), select (2),
@@ -78,11 +78,11 @@ public:
 
 
     /**
-        * Ctor with a continuator (instead of _maxGen) and a eoGenOp.
-        * @param _continuator stopping criteria
-        * @param _eval evaluation function
-        * @param _op variation operator
-       */
+     * Ctor with a continuator (instead of _maxGen) and a eoGenOp.
+     * @param _continuator stopping criteria
+     * @param _eval evaluation function
+     * @param _op variation operator
+     */
     moeoNSGAII (eoContinue < MOEOT > & _continuator, eoEvalFunc < MOEOT > & _eval, eoGenOp < MOEOT > & _op) :
             continuator(_continuator), popEval(_eval), select(2),
             replace(fitnessAssignment, diversityAssignment), genBreed(select, _op), breed(genBreed)
@@ -94,7 +94,7 @@ public:
      * @param _continuator stopping criteria
      * @param _eval evaluation function
      * @param _op variation operator
-    */
+     */
     moeoNSGAII (eoContinue < MOEOT > & _continuator, eoEvalFunc < MOEOT > & _eval, eoTransform < MOEOT > & _op) :
             continuator(_continuator), popEval(_eval), select(2),
             replace(fitnessAssignment, diversityAssignment), genBreed(select, _op), breed(genBreed)
@@ -102,7 +102,7 @@ public:
 
 
     /**
-     * Apply a few generation of evolution to the population _pop.
+     * Apply a few generation of evolution to the population _pop until the stopping criteria is verified.
      * @param _pop the population
      */
     virtual void operator () (eoPop < MOEOT > &_pop)
@@ -136,11 +136,11 @@ protected:
     moeoDetTournamentSelect < MOEOT > select;
     /** fitness assignment used in NSGA-II */
     moeoFastNonDominatedSortingFitnessAssignment < MOEOT > fitnessAssignment;
-    /** Diversity assignment used in NSGA-II */
+    /** diversity assignment used in NSGA-II */
     moeoCrowdingDistanceDiversityAssignment  < MOEOT > diversityAssignment;
     /** elitist replacement */
     moeoElitistReplacement < MOEOT > replace;
-    /** a object for genetic operators (used as default) */
+    /** an object for genetic operators (used as default) */
     eoSGAGenOp < MOEOT > defaultSGAGenOp;
     /** general breeder */
     eoGeneralBreeder < MOEOT > genBreed;
