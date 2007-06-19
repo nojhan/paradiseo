0911feb4f7db81641721f66fc5d8ea99ab0c6a92
liefooga <liefooga@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-06-19 09:29:05 +0000

    add the aggregative scheme
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@335 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/paradiseo-moeo-1.0/src/moeoComparator.h
+++ branches/paradiseo-moeo-1.0/src/moeoComparator.h
@@ -144,4 +144,39 @@ public:
 };
 
 
+/**
+ * Functor allowing to compare two solutions according to their fitness and diversity values, each according to its aggregative value.
+ */
+template < class MOEOT >
+class moeoAggregativeComparator : public moeoComparator < MOEOT >
+{
+public:
+
+    /**
+        * Ctor.
+        * @param _weightFitness the weight for fitness
+        * @param _weightDiversity the weight for diversity
+        */
+    moeoAggregativeComparator(double _weightFitness = 1.0, double _weightDiversity = 1.0) : weightFitness(_weightFitness), weightDiversity(_weightDiversity)
+    {}
+
+    /**
+     * Returns true if _moeo1 < _moeo2 according to the aggregation of their fitness and diversity values
+     * @param _moeo1 the first solution
+     * @param _moeo2 the second solution
+     */
+    const bool operator()(const MOEOT & _moeo1, const MOEOT & _moeo2)
+    {
+        return ( weightFitness * _moeo1.fitness() + weightDiversity * _moeo1.diversity() ) < ( weightFitness * _moeo2.fitness() + weightDiversity * _moeo2.diversity() );
+    }
+
+private:
+    /** the weight for fitness */
+    double weightFitness;
+    /** the weight for diversity */
+    double weightDiversity;
+
+};
+
+
 #endif /*MOEOCOMPARATOR_H_*/
