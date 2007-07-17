87cb8e0a6fd58322390a637843e6c28745e1a8c7
liefooga <liefooga@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-07-17 09:00:45 +0000

    new abstract function
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@576 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-moeo/src/fitness/moeoBinaryIndicatorBasedFitnessAssignment.h
+++ trunk/paradiseo-moeo/src/fitness/moeoBinaryIndicatorBasedFitnessAssignment.h
@@ -19,6 +19,22 @@
  * moeoIndicatorBasedFitnessAssignment for binary indicators.
  */
 template < class MOEOT >
-class moeoBinaryIndicatorBasedFitnessAssignment : public moeoIndicatorBasedFitnessAssignment < MOEOT > {};
+class moeoBinaryIndicatorBasedFitnessAssignment : public moeoIndicatorBasedFitnessAssignment < MOEOT >
+{
+public:
+
+    /** The type for objective vector */
+    typedef typename MOEOT::ObjectiveVector ObjectiveVector;
+
+
+    /**
+     * Updates the fitness values of the whole population _pop by taking the new objective vector _objVec into account 
+     * and returns the fitness value of _objVec.
+     * @param _pop the population
+     * @param _objVec the objective vector
+     */
+    virtual double updateByAdding(eoPop < MOEOT > & _pop, ObjectiveVector & _objVec) = 0;
+
+};
 
 #endif /*MOEOINDICATORBASEDFITNESSASSIGNMENT_H_*/
