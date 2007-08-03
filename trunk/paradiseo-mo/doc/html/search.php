7a47863fd2d0daa6f6cae4be1868d203c49d54ca
jboisson <jboisson@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-08-03 14:40:30 +0000

    I have forgotten to regenerate the documentation, it is done
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@589 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-mo/doc/html/search.php
+++ trunk/paradiseo-mo/doc/html/search.php
@@ -374,7 +374,7 @@ main();
 
 
 ?>
-<hr size="1"><address style="text-align: right;"><small>Generated on Fri Aug 3 16:25:56 2007 for PARADISEO-MO by&nbsp;
+<hr size="1"><address style="text-align: right;"><small>Generated on Fri Aug 3 16:38:57 2007 for PARADISEO-MO by&nbsp;
 <a href="http://www.doxygen.org/index.html">
 <img src="doxygen.png" alt="doxygen" align="middle" border="0"></a> 1.5.2 </small></address>
 </body>
