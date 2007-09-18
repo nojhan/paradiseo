18a029bda5c381d5fae09c7df2a33620d1c7f385
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-09-18 15:57:47 +0000

    updated cmake config
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@595 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-moeo/moeo-conf.cmake
+++ trunk/paradiseo-moeo/moeo-conf.cmake
@@ -2,31 +2,24 @@
 ######################################################################################
 ######################################################################################
 ###  In this file, you can specify many CMake variables used to build paradisEO-MOEO.
-###  For example, if you don't want ot give the EO path each time on the command line,
-###  uncomment the line the "SET(EO_DIR...)" and set your path.
-###  The section numbers are the same as those used in the CMakeLists.txt file.
 ######################################################################################
 ######################################################################################
 
+
 ######################################################################################
-### 3) OPTIONNAL - Overwrite default paths
+### 1) OPTIONNAL 
 ######################################################################################
 
-# SET(MOEO_DIR "<your path>" CACHE PATH "ParadisEO-MOEO main directory") 
-# SET(EO_DIR "<path to ParadisEO-EO>" CACHE PATH "ParadisEO-EO main directory")
-
-# SET(EO_SRC_DIR "<path to ParadisEO-EO src dir>")
-# SET(MOEO_SRC_DIR "<path to ParadisEO-MOEO src dir>")
-# SET(MOEO_DOC_DIR "<path to ParadisEO-MOEO doc dir>")
+# SET (MYVAR MYVALUE)
 
 ######################################################################################
 
 
 #####################################################################################
-### 5) OPTIONNAL - Overwrite subdirs
+### 2) OPTIONNAL - Overwrite subdirs
 ######################################################################################
 
-# SUBDIRS(doc src tutorial)
+# SUBDIRS(doc tutorial)
 
 ######################################################################################
 
