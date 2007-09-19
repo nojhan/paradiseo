5461c361682ec013da177d6a9ddefa3677488946
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-09-19 12:09:03 +0000

    added cmake install config
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@600 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ trunk/install.cmake
@@ -0,0 +1,15 @@
+
+##########################################################################################################################################
+### ParadisEO Install Configuration 
+### The path to the modules EO and MO must be specified above.
+##########################################################################################################################################
+
+SET(EO_SRC_DIR "${CMAKE_SOURCE_DIR}/../paradiseo-eo" CACHE PATH "ParadisEO-EO source directory" FORCE)
+SET(EO_BIN_DIR "${CMAKE_BINARY_DIR}/../../paradiseo-eo/build" CACHE PATH "ParadisEO-EO binary directory" FORCE)
+    
+SET(MO_SRC_DIR "/data/OPAC/software/paradisEO/workspaces/main_WS/test/paradiseo-mo" CACHE PATH "ParadisEO-MO source directory" FORCE)
+SET(MO_BIN_DIR "/data/OPAC/software/paradisEO/workspaces/main_WS/test/paradiseo-mo/build" CACHE PATH "ParadisEO-MO binary directory" FORCE)
+
+###########################################################################################################################################
+    
+    
