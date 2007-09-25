4429a00f4d8bf6963dd9e32c18dc7297ebe484b7
jboisson <jboisson@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-09-25 14:24:14 +0000

    Licence is added on all the files .cpp and .h plus mo, documentation is updated too
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@615 331e1502-861f-0410-8da2-ba01fb791d7f

--- tags/paradiseo-ix86-1.0/paradiseo-mo/src/index.h
+++ tags/paradiseo-ix86-1.0/paradiseo-mo/src/index.h
@@ -1,20 +1,90 @@
+/* <index.h>  
+ *
+ * Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
+ * (C) OPAC Team, LIFL, 2002-2007
+ *
+ * Jean-Charles BOISSON: Jean-Charles.Boisson@lifl.fr
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ * ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+ */
+
 /** @mainpage Welcome to PARADISEO-Moving Objects
 
-@section intro Introduction
+@section Introduction
 
 MO is an extension of the ANSI-C++ compliant evolutionary computation library EO.
 <br>
 It contains classes for almost any kind of one solution based heuristics.
 
-@section tutorial Tutorial
+@section authors AUTHORS
 
+<TABLE>
+<TR>
+  <TD>Sebastien CAHON</TD>
+</TR>
+<TR>
+  <TD><A href=http://www.lifl.fr/~boisson TARGET=blank>Jean-Charles BOISSON</A></TD>
+</TR>
+</TABLE>
 
-@section install Installation
+@section LICENCE
+
+This software is governed by the CeCILL license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+<A href=http://www.cecill.info>http://www.cecill.info</A>.
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+
+@section Paradiseo Home Page
+
+<A href=http://paradiseo.gforge.inria.fr>http://paradiseo.gforge.inria.fr</A>
+
+@section Installation
 
 The installation procedure of the package is detailed in the 
 <a href="../../README">README</a> file in the top-directory of the source-tree.
-
-@section design Overall Design
 
 */
 
