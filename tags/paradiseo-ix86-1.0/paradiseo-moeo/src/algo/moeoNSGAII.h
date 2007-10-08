d0e0c214b3f0c8225de83b0eaf8da0e6e77a6631
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-10-08 15:00:09 +0000

    corrected author list
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@687 331e1502-861f-0410-8da2-ba01fb791d7f

--- tags/paradiseo-ix86-1.0/paradiseo-moeo/src/algo/moeoNSGAII.h
+++ tags/paradiseo-ix86-1.0/paradiseo-moeo/src/algo/moeoNSGAII.h
@@ -3,7 +3,7 @@
 * Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
 * (C) OPAC Team, LIFL, 2002-2007
 *
-* Arnaud Liefooghe
+* AbdelHakim Deneche, Arnaud Liefooghe
 *
 * This software is governed by the CeCILL license under French law and
 * abiding by the rules of distribution of free software.  You can  use,
