159708a948050415576aed1635b2c110e7aaa149
canape <canape@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-10-16 08:17:39 +0000

    Lesson pso
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@730 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-peo/tutorial/Lesson4/main.cpp
+++ trunk/paradiseo-peo/tutorial/Lesson4/main.cpp
@@ -1,3 +1,39 @@
+/* 
+* <main.cpp>
+* Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
+* (C) OPAC Team, LIFL, 2002-2007
+*
+* Clive Canape
+*
+* This software is governed by the CeCILL license under French law and
+* abiding by the rules of distribution of free software.  You can  use,
+* modify and/ or redistribute the software under the terms of the CeCILL
+* license as circulated by CEA, CNRS and INRIA at the following URL
+* "http://www.cecill.info".
+*
+* As a counterpart to the access to the source code and  rights to copy,
+* modify and redistribute granted by the license, users are provided only
+* with a limited warranty  and the software's author,  the holder of the
+* economic rights,  and the successive licensors  have only  limited liability.
+*
+* In this respect, the user's attention is drawn to the risks associated
+* with loading,  using,  modifying and/or developing or reproducing the
+* software by the user in light of its specific status of free software,
+* that may mean  that it is complicated to manipulate,  and  that  also
+* therefore means  that it is reserved for developers  and  experienced
+* professionals having in-depth computer knowledge. Users are therefore
+* encouraged to load and test the software's suitability as regards their
+* requirements in conditions enabling the security of their systems and/or
+* data to be ensured and,  more generally, to use and operate it in the
+* same conditions as regards security.
+* The fact that you are presently reading this means that you have had
+* knowledge of the CeCILL license and that you accept its terms.
+*
+* ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+* Contact: paradiseo-help@lists.gforge.inria.fr
+*          clive.canape@inria.fr
+*/
+
 //For define a parallel PSO or a sequential PSO
 #define PARA
 
@@ -8,7 +44,7 @@ typedef eoRealParticle < double >Indi;
 
 double f (const Indi & _indi)
 {  
-    //Levy function f* = -21.502356 x=(1,1,1,-9.752356 ) for size = 4
+    //Levy function f* = -21.502356 x*=(1,1,1,-9.752356 ) for vector size = 4
     
     const double PI = 4.0 * atan(1.0);
     double sum=0.;
@@ -86,5 +122,5 @@ int main (int __argc, char *__argv[])
     peo :: run(); 
     peo :: finalize();
 #endif
-    std::cout << "Population finale :\n" << pop << std::endl;
+    std::cout << "Final population :\n" << pop << std::endl;
 }
