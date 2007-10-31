a0fe4f8a456cc36f39ceab10979f5a69b6fa82fe
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-10-31 14:13:07 +0000

    tag mySimpleApplication-1.0 done
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@750 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ utilities/tags/mySimpleApplication-1.0/mySimpleApplication.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+// #include < file >
+
+// 	Use functions from namespace std ?
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	// Your code ...
+	cout << "I'm a simple application linked with ParadisEO" << endl ;	
+    
+    return 1;
+    
+}
+
