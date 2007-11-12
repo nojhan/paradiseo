1100fbdb90b458e7efe71c0a02ecd10c5f09c49a
canape <canape@331e1502-861f-0410-8da2-ba01fb791d7f>
2007-11-12 08:48:41 +0000

    New lesson
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@782 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ trunk/paradiseo-peo/tutorial/examples/tsp/two_opt_incr_eval.cpp
@@ -0,0 +1,52 @@
+/* 
+* <two_opt_incr_eval.cpp>
+* Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
+* (C) OPAC Team, LIFL, 2002-2007
+*
+* Sebastien Cahon, Alexandru-Adrian Tantar
+*
+* This software is governed by the CeCILL license under French law and
+* abiding by the rules of distribution of free software.  You can  use,
+* modify and/ or redistribute the software under the terms of the CeCILL
+* license as circulated by CEA, CNRS and INRIA at the following URL
+* "http://www.cecill.info".
+*
+* As a counterpart to the access to the source code and  rights to copy,
+* modify and redistribute granted by the license, users are provided only
+* with a limited warranty  and the software's author,  the holder of the
+* economic rights,  and the successive licensors  have only  limited liability.
+*
+* In this respect, the user's attention is drawn to the risks associated
+* with loading,  using,  modifying and/or developing or reproducing the
+* software by the user in light of its specific status of free software,
+* that may mean  that it is complicated to manipulate,  and  that  also
+* therefore means  that it is reserved for developers  and  experienced
+* professionals having in-depth computer knowledge. Users are therefore
+* encouraged to load and test the software's suitability as regards their
+* requirements in conditions enabling the security of their systems and/or
+* data to be ensured and,  more generally, to use and operate it in the
+* same conditions as regards security.
+* The fact that you are presently reading this means that you have had
+* knowledge of the CeCILL license and that you accept its terms.
+*
+* ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+* Contact: paradiseo-help@lists.gforge.inria.fr
+*
+*/
+
+#include "two_opt_incr_eval.h"
+#include "node.h"
+
+int TwoOptIncrEval :: operator () (const TwoOpt & __move, const Route & __route) {
+  
+  /* From */
+  Node v1 = __route [__move.first], v1_left = __route [(__move.first - 1 + numNodes) % numNodes];
+  
+  /* To */
+  Node v2 = __route [__move.second], v2_right = __route [(__move.second + 1) % numNodes];
+ 
+  if (v1 == v2 || v2_right == v1)
+    return __route.fitness ();
+  else 
+    return __route.fitness () - distance (v1_left, v2) - distance (v1, v2_right) + distance (v1_left, v1) + distance (v2, v2_right);
+}
