135442f35076a477002763ad29fc987c4e01614b
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2008-01-17 13:12:00 +0000

    generic island model OK !
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@893 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ trunk/paradiseo-peo/src/peoData.h
@@ -0,0 +1,37 @@
+
+
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+//-----------------------------------------------------------------------------
+// peoData.h
+// (c) GeNeura Team, 1998
+/*
+ */
+//-----------------------------------------------------------------------------
+
+#ifndef _PEODATA_H
+#define _PEODATA_H
+
+
+class peoData {
+public:
+
+  virtual void pack () {}
+  virtual void unpack () {}
+  
+};
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
