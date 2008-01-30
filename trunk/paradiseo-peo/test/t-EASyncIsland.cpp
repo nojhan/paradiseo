743d94aea0b1f8e7a1a0bb24f5d4d0e3c0367f2d
canape <canape@331e1502-861f-0410-8da2-ba01fb791d7f>
2008-01-30 13:49:55 +0000

    Test peo modified
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@911 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-peo/test/t-EASyncIsland.cpp
+++ trunk/paradiseo-peo/test/t-EASyncIsland.cpp
@@ -3,6 +3,6 @@
 int main (int __argc, char *__argv[])
 {
 	system("mpdboot");
-	system("mpiexec -n 4 ./t-ParallelEvalLib @param ");
+	system("mpiexec -n 4 ./t-EASyncIslandLib @param ");
 	system("mpdallexit");
 }
--- trunk/paradiseo-peo/test/t-MultiStart.cpp
+++ trunk/paradiseo-peo/test/t-MultiStart.cpp
@@ -1,40 +1,8 @@
-// Test : multistart
+#include <peo.h>
 
-#include <peo>
-
-struct Algorithm 
+int main (int __argc, char *__argv[])
 {
-	void operator()(double & _d) 
-	{
-		_d = _d * _d;	
-	}
-};
-
-int main (int __argc, char * * __argv)
-{
-
-  peo :: init (__argc, __argv);
-  if (getNodeRank()==1)
-  	std::cout<<"\n\nTest : multistart\n\n";
-  std::vector < double > v;
-  if (getNodeRank()==1)
-  	std::cout<<"\n\nBefore :";
-  for(unsigned i = 0; i< 10; i++)
-  {
-  	v.push_back(i);
- 	if (getNodeRank()==1)
-  		std::cout<<"\n"<<v[i];
-  }
-  Algorithm algo;
-  peoMultiStart < double > initParallel (algo);
-  peoWrapper parallelAlgo (initParallel, v);
-  initParallel.setOwner(parallelAlgo);
-  peo :: run( );
-  peo :: finalize( );
-  if (getNodeRank()==1)
-  {
-  	std::cout<<"\n\nAfter :\n";
-  	for(unsigned i = 0; i< 10; i++)
-		std::cout<<v[i]<<"\n";
-  }
+	system("mpdboot");
+	system("mpiexec -n 4 ./t-MultiStartLib @param ");
+	system("mpdallexit");
 }
