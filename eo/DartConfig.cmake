60e8d2bc0f729e77057a668ab93e0dc997b11dea
tlegrand <tlegrand>
2008-02-14 08:39:41 +0000

    Added DartConfig for Dart reporting. By default CTest will submit to dart.irisa.fr (project ParadisEO).

---
+++ eo/DartConfig.cmake
@@ -0,0 +1,11 @@
+
+#############################################################################
+# Dart config for report submission
+#############################################################################
+
+SET (DROP_METHOD "xmlrpc")
+SET (DROP_SITE "http://dart.irisa.fr")
+SET (DROP_LOCATION "ParadisEO")
+SET (CVS_WEB_URL "http://eodev.cvs.sourceforge.net/eodev/")
+SET (CVS_WEB_CVSROOT "eo")
+#############################################################################
