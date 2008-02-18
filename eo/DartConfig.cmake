b08b22f172f67f73a47a9e9696c9074e9261883c
tlegrand <tlegrand>
2008-02-18 15:08:08 +0000

    added nighlty start date for Dart config

--- eo/DartConfig.cmake
+++ eo/DartConfig.cmake
@@ -8,4 +8,5 @@ SET (DROP_SITE "http://dart.irisa.fr")
 SET (DROP_LOCATION "ParadisEO")
 SET (CVS_WEB_URL "http://eodev.cvs.sourceforge.net/eodev/")
 SET (CVS_WEB_CVSROOT "eo")
+SET (NIGHTLY_START_TIME "23:00:00 EDT")
 #############################################################################
