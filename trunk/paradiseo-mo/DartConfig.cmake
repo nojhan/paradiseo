79759e68c396b36caf26e2036b4a2f5f4c73005e
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2008-02-20 14:29:25 +0000

    dummy modif to see if dart works ...
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@974 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-mo/DartConfig.cmake
+++ trunk/paradiseo-mo/DartConfig.cmake
@@ -6,5 +6,5 @@
 SET (DROP_METHOD "xmlrpc")
 SET (DROP_SITE "http://dart.irisa.fr")
 SET (DROP_LOCATION "ParadisEO")
-SET (NIGHTLY_START_TIME "23:00:00 WEST")
+SET (NIGHTLY_START_TIME "23:00:00 WEST") 
 #############################################################################
