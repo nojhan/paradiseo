ad5e398ff39a47a5369b15cb39118dd0ceb9021b
tlegrand <tlegrand>
2008-03-11 13:16:35 +0000

    changed dart server

--- eo/DartConfig.cmake
+++ eo/DartConfig.cmake
@@ -4,9 +4,10 @@
 #############################################################################
 
 SET (DROP_METHOD "xmlrpc")
-SET (DROP_SITE "http://dart.irisa.fr")
+SET (DROP_SITE "http://dart-test.irisa.fr")
 SET (DROP_LOCATION "ParadisEO")
 SET (CVS_WEB_URL "http://eodev.cvs.sourceforge.net/eodev/")
 SET (CVS_WEB_CVSROOT "eo")
 SET (NIGHTLY_START_TIME "23:00:00 WEST")
+
 #############################################################################
