ca30a98d20990bd67a787fb6eb80a4d7cac8b2e7
legrand <legrand@331e1502-861f-0410-8da2-ba01fb791d7f>
2008-03-20 10:16:20 +0000

    started to fill patch1 tag using the tag paradiseo-1.1 and the installs script located in the trunk
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@1175 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ tags/paradiseo-1.1-patch1/paradiseo-peo/src/peoPopEval.h
@@ -0,0 +1,260 @@
+/*
+* <peoPopEval.h>
+* Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2008
+* (C) OPAC Team, LIFL, 2002-2008
+*
+* Sebastien Cahon, Alexandru-Adrian Tantar, Clive Canape
+*
+* This software is governed by the CeCILL license under French law and
+* abiding by the rules of distribution of free software.  You can  use,
+* modify and/ or redistribute the software under the terms of the CeCILL
+* license as circulated by CEA, CNRS and INRIA at the following URL
+* "http://www.cecill.info".
+*
+* As a counterpart to the access to the source code and  rights to copy,
+* modify and redistribute granted by the license, users are provided only
+* with a limited warranty  and the software's author,  the holder of the
+* economic rights,  and the successive licensors  have only  limited liability.
+*
+* In this respect, the user's attention is drawn to the risks associated
+* with loading,  using,  modifying and/or developing or reproducing the
+* software by the user in light of its specific status of free software,
+* that may mean  that it is complicated to manipulate,  and  that  also
+* therefore means  that it is reserved for developers  and  experienced
+* professionals having in-depth computer knowledge. Users are therefore
+* encouraged to load and test the software's suitability as regards their
+* requirements in conditions enabling the security of their systems and/or
+* data to be ensured and,  more generally, to use and operate it in the
+* same conditions as regards security.
+* The fact that you are presently reading this means that you have had
+* knowledge of the CeCILL license and that you accept its terms.
+*
+* ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+* Contact: paradiseo-help@lists.gforge.inria.fr
+*
+*/
+
+#ifndef __peoPopEval_h
+#define __peoPopEval_h
+
+#include <queue>
+#include <eoEvalFunc.h>
+
+#include "core/messaging.h"
+#include "core/peo_debug.h"
+#include "peoAggEvalFunc.h"
+#include "peoNoAggEvalFunc.h"
+
+//! @class peoPopEval
+//! @brief Parallel evaluation functor wrapper
+//! @see Service eoPopEvalFunc
+//! @version 1.2
+//! @date 2006
+template< class EOT > class peoPopEval : public Service, public eoPopEvalFunc<EOT>
+  {
+
+  public:
+
+    //! Constructor function - an EO-derived evaluation functor has to be specified; an internal reference
+    //! is set towards the specified evaluation functor.
+    //!
+    //! @param eoEvalFunc< EOT >& __eval_func - EO-derived evaluation functor to be applied in parallel on each individual of a specified population
+    peoPopEval( eoEvalFunc< EOT >& __eval_func );
+
+    //! Constructor function - a vector of EO-derived evaluation functors has to be specified as well as an aggregation function.
+    //!
+    //! @param const std :: vector< eoEvalFunc < EOT >* >& __funcs - vector of EO-derived partial evaluation functors;
+    //! @param peoAggEvalFunc< EOT >& __merge_eval - aggregation functor for creating a fitness value out of the partial fitness values.
+    peoPopEval( const std :: vector< eoEvalFunc < EOT >* >& __funcs, peoAggEvalFunc< EOT >& __merge_eval );
+
+    //! Operator for applying the evaluation functor (direct or aggregate) for each individual of the specified population.
+    //!
+    //! @param eoPop< EOT >& __pop - population to be evaluated by applying the evaluation functor specified in the constructor.
+    void operator()(eoPop< EOT >& __pop);
+
+    //! @brief Operator ()( eoPop< EOT >& __dummy, eoPop< EOT >& __pop )
+    //! @param eoPop< EOT >& __dummy
+    //! @param eoPop< EOT >& __pop
+    void operator()( eoPop< EOT >& __dummy, eoPop< EOT >& __pop );
+
+    //! Auxiliary function for transferring data between the process requesting an evaluation operation and the process that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void packData();
+
+    //! Auxiliary function for transferring data between the process requesting an evaluation operation and the process that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void unpackData();
+
+    //! Auxiliary function - it calls the specified evaluation functor(s). There is no need to explicitly call the function.
+    void execute();
+
+    //! Auxiliary function for transferring data between the process requesting an evaluation operation and the process that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void packResult();
+
+    //! Auxiliary function for transferring data between the process requesting an evaluation operation and the process that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void unpackResult();
+
+    //! Auxiliary function for notifications between the process requesting an evaluation operation and the processes that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void notifySendingData();
+
+    //! Auxiliary function for notifications between the process requesting an evaluation operation and the processes that
+    //! performs the actual evaluation phase. There is no need to explicitly call the function.
+    void notifySendingAllResourceRequests();
+
+  private:
+
+    //! @param std :: vector< eoEvalFunc < EOT >* >& funcs
+    //! @param std :: vector< eoEvalFunc < EOT >* > one_func
+    //! @param peoAggEvalFunc< EOT >& merge_eval
+    //! @param peoNoAggEvalFunc< EOT > no_merge_eval
+    //! @param std :: queue< EOT* >tasks
+    //! @param std :: map< EOT*, std :: pair< unsigned, unsigned > > progression
+    //! @param unsigned num_func
+    //! @param EOT sol
+    //! @param EOT *ad_sol
+    //! @param unsigned total
+    const std :: vector< eoEvalFunc < EOT >* >& funcs;
+    std :: vector< eoEvalFunc < EOT >* > one_func;
+    peoAggEvalFunc< EOT >& merge_eval;
+    peoNoAggEvalFunc< EOT > no_merge_eval;
+    std :: queue< EOT* >tasks;
+    std :: map< EOT*, std :: pair< unsigned, unsigned > > progression;
+    unsigned num_func;
+    EOT sol;
+    EOT *ad_sol;
+    unsigned total;
+  };
+
+
+template< class EOT > peoPopEval< EOT > :: peoPopEval( eoEvalFunc< EOT >& __eval_func ) :
+
+    funcs( one_func ), merge_eval( no_merge_eval )
+{
+
+  one_func.push_back( &__eval_func );
+}
+
+
+template< class EOT > peoPopEval< EOT > :: peoPopEval(
+
+  const std :: vector< eoEvalFunc< EOT >* >& __funcs,
+  peoAggEvalFunc< EOT >& __merge_eval
+
+) : funcs( __funcs ), merge_eval( __merge_eval )
+{}
+
+template< class EOT > void peoPopEval< EOT >::operator()(eoPop< EOT >& __dummy, eoPop< EOT >& __pop )
+{
+  this->operator()(__pop);
+}
+
+template< class EOT > void peoPopEval< EOT >::operator()(eoPop< EOT >& __pop )
+{
+  if ( __pop.size() && (funcs.size() * __pop.size()) )
+    {
+      for ( unsigned i = 0; i < __pop.size(); i++ )
+        {
+          __pop[ i ].fitness(typename EOT :: Fitness() );
+          progression[ &__pop[ i ] ].first = funcs.size() - 1;
+          progression[ &__pop[ i ] ].second = funcs.size();
+          for ( unsigned j = 0; j < funcs.size(); j++ )
+            {
+              /* Queuing the 'invalid' solution and its associated owner */
+              tasks.push( &__pop[ i ] );
+            }
+        }
+      total = funcs.size() * __pop.size();
+      requestResourceRequest( funcs.size() * __pop.size() );
+      stop();
+    }
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: packData()
+{
+  //  printDebugMessage ("debut pakc data");
+  pack( progression[ tasks.front() ].first-- );
+
+  /* Packing the contents :-) of the solution */
+  pack( *tasks.front() );
+
+  /* Packing the addresses of both the solution and the owner */
+  pack( tasks.front() );
+  tasks.pop(  );
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: unpackData()
+{
+  unpack( num_func );
+  /* Unpacking the solution */
+  unpack( sol );
+  /* Unpacking the @ of that one */
+  unpack( ad_sol );
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: execute()
+{
+
+  /* Computing the fitness of the solution */
+  funcs[ num_func ]->operator()( sol );
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: packResult()
+{
+  /* Packing the fitness of the solution */
+  pack( sol.fitness() );
+  /* Packing the @ of the individual */
+  pack( ad_sol );
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: unpackResult()
+{
+  typename EOT :: Fitness fit;
+
+  /* Unpacking the computed fitness */
+  unpack( fit );
+
+  /* Unpacking the @ of the associated individual */
+  unpack( ad_sol );
+
+
+  /* Associating the fitness the local solution */
+  merge_eval( *ad_sol, fit );
+
+  progression[ ad_sol ].second--;
+
+  /* Notifying the container of the termination of the evaluation */
+  if ( !progression[ ad_sol ].second )
+    {
+
+      progression.erase( ad_sol );
+    }
+
+  total--;
+  if ( !total )
+    {
+
+      getOwner()->setActive();
+      resume();
+    }
+}
+
+
+template< class EOT > void peoPopEval< EOT > :: notifySendingData()
+{}
+
+
+template< class EOT > void peoPopEval< EOT > :: notifySendingAllResourceRequests()
+{
+  getOwner()->setPassive();
+}
+
+
+#endif
---
+++ tags/paradiseo-1.1-patch1/paradiseo-peo/tutorial/Lesson3/mainPSO.cpp
@@ -0,0 +1,166 @@
+/*
+* <mainPSO.cpp>
+* Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2008
+* (C) OPAC Team, INRIA, 2008
+*
+* Clive Canape
+*
+* This software is governed by the CeCILL license under French law and
+* abiding by the rules of distribution of free software.  You can  use,
+* modify and/ or redistribute the software under the terms of the CeCILL
+* license as circulated by CEA, CNRS and INRIA at the following URL
+* "http://www.cecill.info".
+*
+* As a counterpart to the access to the source code and  rights to copy,
+* modify and redistribute granted by the license, users are provided only
+* with a limited warranty  and the software's author,  the holder of the
+* economic rights,  and the successive licensors  have only  limited liability.
+*
+* In this respect, the user's attention is drawn to the risks associated
+* with loading,  using,  modifying and/or developing or reproducing the
+* software by the user in light of its specific status of free software,
+* that may mean  that it is complicated to manipulate,  and  that  also
+* therefore means  that it is reserved for developers  and  experienced
+* professionals having in-depth computer knowledge. Users are therefore
+* encouraged to load and test the software's suitability as regards their
+* requirements in conditions enabling the security of their systems and/or
+* data to be ensured and,  more generally, to use and operate it in the
+* same conditions as regards security.
+* The fact that you are presently reading this means that you have had
+* knowledge of the CeCILL license and that you accept its terms.
+*
+* ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+* Contact: paradiseo-help@lists.gforge.inria.fr
+*
+*/
+
+#include <peo>
+
+typedef eoRealParticle < double >Indi;
+
+double f (const Indi & _indi)
+{
+  double sum;
+  sum=_indi[1]-pow(_indi[0],2);
+  sum=100*pow(sum,2);
+  sum+=pow((1-_indi[0]),2);
+  return (-sum);
+}
+
+int main (int __argc, char *__argv[])
+{
+  peo :: init( __argc, __argv );
+  eoParser parser(__argc, __argv);
+  unsigned int POP_SIZE = parser.createParam((unsigned int)(20), "popSize", "Population size",'P',"Param").value();
+  unsigned int MAX_GEN = parser.createParam((unsigned int)(100), "maxGen", "Maximum number of generations",'G',"Param").value();
+  unsigned int VEC_SIZE = parser.createParam((unsigned int)(2), "vecSize", "Vector size",'V',"Param").value();
+  double INIT_POSITION_MIN = parser.createParam(-2.0, "pMin", "Init position min",'N',"Param").value();
+  double INIT_POSITION_MAX = parser.createParam(2.0, "pMax", "Init position max",'X',"Param").value();
+  double INIT_VELOCITY_MIN = parser.createParam(-1.0, "vMin", "Init velocity min",'n',"Param").value();
+  double INIT_VELOCITY_MAX = parser.createParam(1.0, "vMax", "Init velocity max",'x',"Param").value();
+  double omega = parser.createParam(1.0, "weight", "Weight",'w',"Param").value();
+  double C1 = parser.createParam(0.5, "c1", "C1",'1',"Param").value();
+  double C2 = parser.createParam(2.0, "c2t", "C2",'2',"Param").value();
+  unsigned int NEIGHBORHOOD_SIZE = parser.createParam((unsigned int)(6), "neighSize", "Neighborhood size",'H',"Param").value();
+  unsigned int MIG_FREQ = parser.createParam((unsigned int)(10), "migFreq", "Migration frequency",'F',"Param").value();
+  rng.reseed (time(0));
+
+// Island model
+
+  RingTopology topologyMig;
+
+// First
+  eoGenContinue < Indi > genContPara (MAX_GEN);
+  eoCombinedContinue <Indi> continuatorPara (genContPara);
+  eoCheckPoint<Indi> checkpoint(continuatorPara);
+  peoEvalFunc<Indi, double, const Indi& > plainEval(f);
+  peoPopEval< Indi > eval(plainEval);
+  eoUniformGenerator < double >uGen (INIT_POSITION_MIN, INIT_POSITION_MAX);
+  eoInitFixedLength < Indi > random (VEC_SIZE, uGen);
+  eoUniformGenerator < double >sGen (INIT_VELOCITY_MIN, INIT_VELOCITY_MAX);
+  eoVelocityInitFixedLength < Indi > veloRandom (VEC_SIZE, sGen);
+  eoFirstIsBestInit < Indi > localInit;
+  eoRealVectorBounds bndsFlight(VEC_SIZE,INIT_POSITION_MIN,INIT_POSITION_MAX);
+  eoStandardFlight < Indi > flight(bndsFlight);
+  eoPop < Indi > pop;
+  pop.append (POP_SIZE, random);
+  eoLinearTopology<Indi> topology(NEIGHBORHOOD_SIZE);
+  eoRealVectorBounds bnds(VEC_SIZE,INIT_VELOCITY_MIN,INIT_VELOCITY_MAX);
+  eoStandardVelocity < Indi > velocity (topology,omega,C1,C2,bnds);
+  eoInitializer <Indi> init(eval,veloRandom,localInit,topology,pop);
+
+// Island model
+
+  eoPeriodicContinue< Indi > mig_cont( MIG_FREQ );
+  peoPSOSelect<Indi> mig_selec(topology);
+  peoWorstPositionReplacement<Indi> mig_replac;
+
+// Specific implementation (peoData.h)
+
+  eoContinuator<Indi> cont(mig_cont, pop);
+  eoSelector <Indi, eoPop<Indi> > mig_select (mig_selec,1,pop);
+  eoReplace <Indi, eoPop<Indi> > mig_replace (mig_replac,pop);
+
+
+// Second
+
+  eoGenContinue < Indi > genContPara2 (MAX_GEN);
+  eoCombinedContinue <Indi> continuatorPara2 (genContPara2);
+  eoCheckPoint<Indi> checkpoint2(continuatorPara2);
+  peoEvalFunc<Indi, double, const Indi& > plainEval2(f);
+  peoPopEval< Indi > eval2(plainEval2);
+  eoUniformGenerator < double >uGen2 (INIT_POSITION_MIN, INIT_POSITION_MAX);
+  eoInitFixedLength < Indi > random2 (VEC_SIZE, uGen2);
+  eoUniformGenerator < double >sGen2 (INIT_VELOCITY_MIN, INIT_VELOCITY_MAX);
+  eoVelocityInitFixedLength < Indi > veloRandom2 (VEC_SIZE, sGen2);
+  eoFirstIsBestInit < Indi > localInit2;
+  eoRealVectorBounds bndsFlight2(VEC_SIZE,INIT_POSITION_MIN,INIT_POSITION_MAX);
+  eoStandardFlight < Indi > flight2(bndsFlight2);
+  eoPop < Indi > pop2;
+  pop2.append (POP_SIZE, random2);
+  eoLinearTopology<Indi> topology2(NEIGHBORHOOD_SIZE);
+  eoRealVectorBounds bnds2(VEC_SIZE,INIT_VELOCITY_MIN,INIT_VELOCITY_MAX);
+  eoStandardVelocity < Indi > velocity2 (topology2,omega,C1,C2,bnds2);
+  eoInitializer <Indi> init2(eval2,veloRandom2,localInit2,topology2,pop2);
+
+// Island model
+
+  eoPeriodicContinue< Indi > mig_cont2( MIG_FREQ );
+  peoPSOSelect<Indi> mig_selec2(topology2);
+  peoWorstPositionReplacement<Indi> mig_replac2;
+
+// Specific implementation (peoData.h)
+
+  eoContinuator<Indi> cont2(mig_cont2,pop2);
+  eoSelector <Indi, eoPop<Indi> > mig_select2 (mig_selec2,1,pop2);
+  eoReplace <Indi, eoPop<Indi> > mig_replace2 (mig_replac2,pop2);
+
+
+// Asynchronous island
+
+  peoAsyncIslandMig< eoPop<Indi>, eoPop<Indi> > mig(cont,mig_select, mig_replace, topologyMig);
+  checkpoint.add( mig );
+  peoAsyncIslandMig< eoPop<Indi>, eoPop<Indi> > mig2(cont2,mig_select2, mig_replace2, topologyMig);
+  checkpoint2.add( mig2 );
+
+
+// Parallel algorithm
+
+  eoSyncEasyPSO <Indi> psa(init,checkpoint,eval, velocity, flight);
+  peoWrapper parallelPSO( psa, pop);
+  eval.setOwner(parallelPSO);
+  mig.setOwner(parallelPSO);
+  eoSyncEasyPSO <Indi> psa2(init2,checkpoint2,eval2, velocity2, flight2);
+  peoWrapper parallelPSO2( psa2, pop2);
+  eval2.setOwner(parallelPSO2);
+  mig2.setOwner(parallelPSO2);
+  peo :: run();
+  peo :: finalize();
+  if (getNodeRank()==1)
+    {
+      pop.sort();
+      pop2.sort();
+      std::cout << "Final population :\n" << pop << std::endl;
+      std::cout << "Final population :\n" << pop2	 << std::endl;
+    }
+}
