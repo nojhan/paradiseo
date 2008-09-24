ea6fce84f21667f031d9154f3544d4a712b72ed3
liefooga <liefooga@331e1502-861f-0410-8da2-ba01fb791d7f>
2008-09-24 15:04:01 +0000

    GCC 4.3 compatible
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@1240 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-moeo/tutorial/examples/flowshop/FlowShopBenchmarkParser.cpp
+++ trunk/paradiseo-moeo/tutorial/examples/flowshop/FlowShopBenchmarkParser.cpp
@@ -34,100 +34,103 @@
 *
 */
 //-----------------------------------------------------------------------------
-
+#include <iostream>
 #include <stdexcept>
 #include <FlowShopBenchmarkParser.h>
 
 FlowShopBenchmarkParser::FlowShopBenchmarkParser(const std::string _benchmarkFileName)
 {
-  init(_benchmarkFileName);
+    init(_benchmarkFileName);
 }
 
 
 const unsigned int FlowShopBenchmarkParser::getM()
 {
-  return M;
+    return M;
 }
 
 
 const unsigned int FlowShopBenchmarkParser::getN()
 {
-  return N;
+    return N;
 }
 
 
 const std::vector< std::vector<unsigned int> > FlowShopBenchmarkParser::getP()
 {
-  return p;
+    return p;
 }
 
 
 const std::vector<unsigned int> FlowShopBenchmarkParser::getD()
 {
-  return d;
+    return d;
 }
 
 
 void FlowShopBenchmarkParser::printOn(std::ostream & _os) const
-  {
+{
     _os << "M=" << M << " N=" << N << std::endl;
     _os << "*** processing times" << std::endl;
     for (unsigned int i=0; i<M; i++)
-      {
+    {
         for (unsigned int j=0; j<N; j++)
-          {
+        {
             _os << p[i][j] << " ";
-          }
+        }
         _os << std::endl;
-      }
+    }
     _os << "*** due-dates" << std::endl;
     for (unsigned int j=0; j<N; j++)
-      {
+    {
         _os << d[j] << " ";
-      }
+    }
     _os << std::endl << std::endl;
-  }
+}
 
 
 void FlowShopBenchmarkParser::init(const std::string _benchmarkFileName)
 {
-  std::string buffer;
-  std::string::size_type start, end;
-  std::ifstream inputFile(_benchmarkFileName.data(), std::ios::in);
-  // opening of the benchmark file
-  if (! inputFile)
-    throw std::runtime_error("*** ERROR : Unable to open the benchmark file");
-  // number of jobs (N)
-  getline(inputFile, buffer, '\n');
-  N = atoi(buffer.data());
-  // number of machines M
-  getline(inputFile, buffer, '\n');
-  M = atoi(buffer.data());
-  // initial and current seeds (not used)
-  getline(inputFile, buffer, '\n');
-  // processing times and due-dates
-  //p = std::vector< std::vector<unsigned int> > (M,N);
-  p.resize(N);
-  d = std::vector<unsigned int> (N);
-  // for each job...
-  for (unsigned int j=0 ; j<N ; j++)
+    std::string buffer;
+    std::string::size_type start, end;
+    std::ifstream inputFile(_benchmarkFileName.data(), std::ios::in);
+    // opening of the benchmark file
+    if (! inputFile)
+        throw std::runtime_error("*** ERROR : Unable to open the benchmark file");
+    // number of jobs (N)
+    getline(inputFile, buffer, '\n');
+    N = atoi(buffer.data());
+    // number of machines M
+    getline(inputFile, buffer, '\n');
+    M = atoi(buffer.data());
+    // initial and current seeds (not used)
+    getline(inputFile, buffer, '\n');
+    // processing times and due-dates
+    // p = std::vector< std::vector<unsigned int> > (M,N);
+    p.resize(M);
+    for (unsigned int j=0 ; j<M ; j++)
     {
-      // index of the job (<=> j)
-      getline(inputFile, buffer, '\n');
-      // due-date of the job j
-      getline(inputFile, buffer, '\n');
-      d[j] = atoi(buffer.data());
-      // processing times of the job j on each machine
-      getline(inputFile, buffer, '\n');
-      start = buffer.find_first_not_of(" ");
-      p[j].resize(M);
-      for (unsigned int i=0 ; i<M ; i++)
+        p[j].resize(N);
+    }
+    d = std::vector<unsigned int> (N);
+    // for each job...
+    for (unsigned int j=0 ; j<N ; j++)
+    {
+        // index of the job (<=> j)
+        getline(inputFile, buffer, '\n');
+        // due-date of the job j
+        getline(inputFile, buffer, '\n');
+        d[j] = atoi(buffer.data());
+        // processing times of the job j on each machine
+        getline(inputFile, buffer, '\n');
+        start = buffer.find_first_not_of(" ");
+        for (unsigned int i=0 ; i<M ; i++)
         {
-          end = buffer.find_first_of(" ", start);
-          p[i][j] = atoi(buffer.substr(start, end-start).data());
-          start = buffer.find_first_not_of(" ", end);
+            end = buffer.find_first_of(" ", start);
+            p[i][j] = atoi(buffer.substr(start, end-start).data());
+            start = buffer.find_first_not_of(" ", end);
         }
     }
-  // closing of the input file
-  inputFile.close();
+    // closing of the input file
+    inputFile.close();
 }
