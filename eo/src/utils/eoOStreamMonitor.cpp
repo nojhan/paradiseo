4a90420dd3807fd569c8886dca059cd34d04c622
Johann Dreo <johann.dreo@thalesgroup.com>
2010-10-26 10:51:48 +0200

    create a more generic class for monitoring to any ostream (useful for output to clog or cerr, for example), the stdout monitor now inherits from it

--- eo/src/utils/eoOStreamMonitor.cpp
+++ eo/src/utils/eoOStreamMonitor.cpp
@@ -6,31 +6,34 @@
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
+#include <iomanip>
+#include <ios>
 
-#include <utils/eoStdoutMonitor.h>
+#include <utils/eoOStreamMonitor.h>
 #include <utils/compatibility.h>
 #include <utils/eoParam.h>
 #include <eoLogger.h>
 
-using namespace std;
+//using namespace std;
 
-eoMonitor& eoStdoutMonitor::operator()(void)
+eoMonitor& eoOStreamMonitor::operator()(void)
 {
-    if (!cout) {
-      string str = "eoStdoutMonitor: Could not write to cout";
-      throw runtime_error (str);
+    if (!out) {
+        std::string str = "eoOStreamMonitor: Could not write to the ooutput stream";
+      throw std::runtime_error(str);
     }
 
     if (firsttime) {
 
       if (verbose) {
-        eo::log << eo::progress << "First Generation" << endl;
+        eo::log << eo::progress << "First Generation" << std::endl;
 
       } else { // else verbose
           for (iterator it = vec.begin (); it != vec.end (); ++it) {
-              cout << (*it)->longName () << delim;
+              out << (*it)->longName ();
+              out << delim << std::left << std::setfill(fill) << std::setw(width);
 	      }
-          cout << endl;
+          out << std::endl;
 	  } // else verbose
 
       firsttime = false;
@@ -40,18 +43,19 @@ eoMonitor& eoStdoutMonitor::operator()(void)
     if (verbose) {
         for (iterator it = vec.begin (); it != vec.end (); ++it) {
             // name: value
-            cout << (*it)->longName () << ": " << (*it)->getValue () << endl;
+            out << (*it)->longName () << ": " << (*it)->getValue () << std::endl;
         } // for it in vec
 
-        eo::log << eo::progress << "End of Generation" << endl;
+        eo::log << eo::progress << "End of Generation" << std::endl;
 
     } else { // else verbose
         for (iterator it = vec.begin (); it != vec.end (); ++it) {
             // value only
-            cout << (*it)->getValue () << delim;
+            out << (*it)->getValue ();
+            out << delim << std::left << std::setfill(fill) << std::setw(width);
         } // for it in vec
 
-        cout << endl;
+        out << std::endl;
     } // if verbose
 
   return *this;
---
+++ eo/src/utils/eoOStreamMonitor.h
@@ -0,0 +1,62 @@
+
+/*
+
+(c) Marc Schoenauer, Maarten Keijzer and GeNeura Team, 2000
+(c) Thales group, 2010
+
+    This library is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free
+    Software Foundation; version 2 of the license.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+    PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along
+    with this library; if not, write to the Free Software Foundation, Inc., 59
+    Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+Contact: http://eodev.sourceforge.net
+
+Authors:
+     todos@geneura.ugr.es
+     Marc.Schoenauer@polytechnique.fr
+     mkeijzer@dhi.dk
+	 Johann Dréo <johann.dreo@thalesgroup.com>
+*/
+
+
+
+#ifndef _eoOStreamMonitor_h_
+#define _eoOStreamMonitor_h_
+
+#include <string>
+#include <iostream>
+
+#include <utils/eoMonitor.h>
+#include <eoObject.h>
+
+/**
+    Prints statistics to stdout
+*/
+class eoOStreamMonitor : public eoMonitor
+{
+public :
+    eoOStreamMonitor( std::ostream & _out, bool _verbose=true, std::string _delim = "\t", unsigned int _width=20, char _fill=' ' ) : 
+        out(_out), verbose(_verbose), delim(_delim), width(_width), fill(_fill), firsttime(true) {}
+
+    eoMonitor& operator()(void);
+
+    virtual std::string className(void) const { return "eoOStreamMonitor"; }
+
+private :
+    std::ostream & out;
+    bool verbose;
+    std::string delim;
+    unsigned int width;
+    char fill;
+    bool firsttime;
+};
+
+#endif // _eoOStreamMonitor_h_
+
