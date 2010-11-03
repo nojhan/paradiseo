b27cbd479cfe47df4843a4ad6c20df69d18f42d6
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2010-11-03 11:01:30 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@1992 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-mo/build/tutorial/Lesson3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/tutorial/Lesson3
-# Build directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/build/tutorial/Lesson3
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- trunk/paradiseo-mo/build/tutorial/Lesson7/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/tutorial/Lesson7
-# Build directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/build/tutorial/Lesson7
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
