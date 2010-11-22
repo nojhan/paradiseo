bb0efdc0671f7bcff39d4b7326f9240ba2ce35f3
Caner Candan <caner.candan@thalesgroup.com>
2010-11-22 17:43:52 +0100

    popStep and dimStep replaced by pS and dS on the results filename

--- eo/test/t-openmp.cpp
+++ eo/test/t-openmp.cpp
@@ -62,8 +62,8 @@ int main(int ac, char** av)
     eoUniformGenerator< double > gen(-5, 5);
 
     std::ostringstream params;
-    params << "--popStep" << popStep << "-p" << popMin << "-P" << popMax
-	   << "--dimStep" << dimStep << "-d" << dimMin << "-D" << dimMax
+    params << "--pS" << popStep << "-p" << popMin << "-P" << popMax
+	   << "--dS" << dimStep << "-d" << dimMin << "-D" << dimMax
 	   << "-r" << nRun << "-s" << seedParam;
     std::ofstream speedupFile( std::string( speedupFileName + params.str() ).c_str() );
     std::ofstream efficiencyFile( std::string( efficiencyFileName + params.str() ).c_str() );
