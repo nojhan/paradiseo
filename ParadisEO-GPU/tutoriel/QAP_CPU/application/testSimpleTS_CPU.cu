10bcbbd157a9b76b804e78f3f6534159ef8a25f6
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-02-08 16:37:33 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2119 331e1502-861f-0410-8da2-ba01fb791d7f

--- ParadisEO-GPU/tutoriel/QAP_CPU/application/testSimpleTS_CPU.cu
+++ ParadisEO-GPU/tutoriel/QAP_CPU/application/testSimpleTS_CPU.cu
@@ -1,11 +1,46 @@
+/*
+  <testSimpleTS_CPU.cu>
+  Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2010
+
+  Karima Boufaras, Thé Van LUONG
+
+  This software is governed by the CeCILL license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited liability.
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+
+  ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+  Contact: paradiseo-help@lists.gforge.inria.fr
+*/
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 using namespace std;
 
-int * a;
-int * b;
-int n;
+//Specific data problem
+unsigned * a;
+unsigned * b;
+unsigned n;
 
 // The general include for eo
 #include <eo>
@@ -15,14 +50,11 @@ int n;
 // Cuda Fitness function
 #include <QapIncrEval.h>
 // QAP solution
-#include <eoVector.h>
 #include <eoInt.h>
-//To compute execution time
-#include <performance/moCudaTimer.h>
+#include <eoInit.h>
 //QAP neighbor
 #include <problems/permutation/moIndexedSwapNeighbor.h>
 //QAP neighborhood 
-//#include <problems/permutation/moSwapNeighborhood.h>
 #include <neighborhood/moOrderNeighborhood.h>
 //QAP data
 #include <Problem.h>
@@ -33,25 +65,19 @@ int n;
 #include <continuator/moIterContinuator.h>
 // Local search algorithm
 #include <algo/moLocalSearch.h>
-// The Tabou Search algorithm explorer
-//#include <explorer/moTSexplorer.h>
 //Algorithm and its components
 #include <algo/moTS.h>
 //Tabu list
-//#include <memory/moNeighborVectorTabuList.h>
 #include <memory/moIndexedVectorTabuList.h>
 //Memories
 #include <memory/moDummyIntensification.h>
 #include <memory/moDummyDiversification.h>
 #include <memory/moBestImprAspiration.h>
-//#include <time.h>
-
-
+//To compute execution time
+#include <performance/moCudaTimer.h>
 
 typedef eoInt<eoMinimizingFitness> solution;
-//typedef moSwapNeighbor<solution> Neighbor;
 typedef moIndexedSwapNeighbor<solution> Neighbor;
-//typedef moSwapNeighborhood<solution> Neighborhood;
 typedef moOrderNeighborhood<Neighbor> Neighborhood;
 
 int main(int argc, char **argv)
@@ -105,6 +131,7 @@ int main(int argc, char **argv)
     ofstream os(statusParam.value().c_str());
     os << parser;// and you can use that file as parameter file
   }
+
   /* =========================================================
    *
    * Random seed
@@ -113,8 +140,7 @@ int main(int argc, char **argv)
 
   //reproducible random seed: if you don't change SEED above,
   // you'll aways get the same result, NOT a random run
-//  rng.reseed(seed);
-  srand(seed);
+  rng.reseed(seed);
   
   /* =========================================================
    *
@@ -129,9 +155,10 @@ int main(int argc, char **argv)
    * Initilisation of the solution
    *
    * ========================================================= */
-
-   solution sol(n);
-   create<solution>(sol);
+   
+  eoInitPermutation<solution> init(n);
+  solution sol;
+  init(sol);
 
   /*=========================================================
    *
