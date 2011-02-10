17fb21e240d99a6393318dadd2eba267cb598e0f
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-02-10 14:05:42 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2158 331e1502-861f-0410-8da2-ba01fb791d7f

---
+++ ParadisEO-GPU/tutoriel/PPP_GPU/application/testSimpleTS_PPP.cu
@@ -0,0 +1,262 @@
+/*
+  <testSimpleTS_PPP.cu>
+  Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2010
+
+  Karima Boufaras, Thé Van LUONG
+
+  This software is governed by the CeCILL license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited liability.
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+
+  ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+  Contact: paradiseo-help@lists.gforge.inria.fr
+*/
+
+//Init the number of threads per block
+#define BLOCK_SIZE 256
+#define Nd 101
+#define Md 117
+#define ca 30
+#define cb 1
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+__device__ int * dev_a;
+__device__ int * dev_h;
+
+// The general include for eo
+#include <eo>
+#include <ga.h>
+// Fitness function
+#include <problems/eval/PPPEval.h>
+// Cuda Fitness function
+#include <eval/moCudaKswapEval.h>
+#include <problems/eval/PPPIncrEval.h>
+//Specific data to PPP problem
+#include <problems/data/PPPData.h>
+// PPP solution
+#include <problems/types/PPPSolution.h>
+// PPP neighbor
+#include <problems/neighborhood/PPPNeighbor.h>
+//To compute execution time
+#include <performance/moCudaTimer.h>
+//Kswap neighborhood
+#include <neighborhood/moCudaKflipNeighborhood.h>
+// The Solution and neighbor comparator
+#include <comparator/moNeighborComparator.h>
+#include <comparator/moSolNeighborComparator.h>
+// The Iter continuator
+#include <continuator/moIterContinuator.h>
+// The Tabou Search algorithm 
+#include <algo/moTS.h>
+//Tabu list
+#include <memory/moIndexedVectorTabuList.h>
+//Memories
+#include <memory/moDummyIntensification.h>
+#include <memory/moDummyDiversification.h>
+#include <memory/moBestImprAspiration.h>
+
+typedef PPPSolution<eoMinimizingFitness> solution;
+typedef PPPNeighbor<solution> Neighbor;
+typedef moCudaKflipNeighborhood<Neighbor> Neighborhood;
+
+
+int main(int argc, char **argv)
+{
+
+  /* =========================================================
+   *
+   * Parameters
+   *
+   * ========================================================= */
+
+  // First define a parser from the command-line arguments
+     eoParser parser(argc, argv);
+
+    // For each parameter, define Parameter, read it through the parser,
+    // and assign the value to the variable
+
+    // seed
+    eoValueParam<uint32_t> seedParam(time(0), "seed", "Random number seed", 'S');
+    parser.processParam( seedParam );
+    unsigned seed = seedParam.value();
+
+    // Swap number
+    eoValueParam<unsigned int> KSwapParam(0, "KSwap", "swap number", 'N');
+    parser.processParam(KSwapParam, "KSwap" );
+    unsigned KSwap = KSwapParam.value();
+
+    // Iteration number
+    eoValueParam<unsigned int> nbIterationParam(1, "nbIteration", "TS Iteration number", 'I');
+    parser.processParam( nbIterationParam, "TS Iteration number" );
+    unsigned nbIteration = nbIterationParam.value();
+
+    // size tabu list
+    eoValueParam<unsigned int> sizeTabuListParam(7, "sizeTabuList", "size of the tabu list", 'T');
+    parser.processParam( sizeTabuListParam, "Search Parameters" );
+    unsigned sizeTabuList = sizeTabuListParam.value();
+
+    // duration tabu list
+    eoValueParam<unsigned int> durationParam(7, "duration", "duration of the tabu list", 'D');
+    parser.processParam( durationParam, "Search Parameters" );
+    unsigned duration = durationParam.value();
+
+    // the name of the "status" file where all actual parameter values will be saved
+    string str_status = parser.ProgramName() + ".status"; // default value
+    eoValueParam<string> statusParam(str_status.c_str(), "status", "Status file");
+     parser.processParam( statusParam, "Persistence" );
+
+   // do the following AFTER ALL PARAMETERS HAVE BEEN PROCESSED
+   // i.e. in case you need parameters somewhere else, postpone these
+   if (parser.userNeedsHelp()) {
+    parser.printHelp(cout);
+    exit(1);
+    }
+    
+    if (statusParam.value() != "") {
+       ofstream os(statusParam.value().c_str());
+       os << parser;// and you can use that file as parameter file
+    }
+
+
+  /* =========================================================
+   *
+   * Random seed
+   *
+   * ========================================================= */
+
+  //reproducible random seed: if you don't change SEED above,
+  // you'll aways get the same result, NOT a random run
+  rng.reseed(seed);
+
+  /* =========================================================
+   *
+   * Initilisation of QAP data
+   *
+   * ========================================================= */
+
+  PPPData<int> _data;
+
+  /* =========================================================
+   *
+   * Initilisation of the solution
+   *
+   * ========================================================= */
+ 
+   solution sol(Nd);
+  _data.cudaObject.memCopyGlobalVariable(dev_a,_data.a_d);
+  _data.cudaObject.memCopyGlobalVariable(dev_h,_data.H_d);
+  
+  /*=========================================================
+   *
+   * Evaluation of a solution neighbor's
+   *
+   * ========================================================= */
+
+    PPPEval<solution> eval(_data);
+    unsigned long int sizeMap=sizeMapping(Nd,KSwap);
+    PPPIncrEval<Neighbor> incr_eval;
+    moCudaKswapEval<Neighbor,PPPIncrEval<Neighbor> > cueval(sizeMap,incr_eval);
+  
+  /* =========================================================
+   *
+   * Comparator of solutions and neighbors
+   *
+   * ========================================================= */
+
+    moNeighborComparator<Neighbor> comparator;
+    moSolNeighborComparator<Neighbor> solComparator;
+
+  /* =========================================================
+   *
+   * a solution neighborhood
+   *
+   * ========================================================= */
+
+     Neighborhood neighborhood(Nd,KSwap,cueval);
+
+  /* =========================================================
+   *
+   * continuator
+   *
+   * ========================================================= */
+
+     moIterContinuator <Neighbor> continuator(nbIteration);
+  
+  /* =========================================================
+   *
+   * tabu list
+   *
+   * ========================================================= */
+
+     sizeTabuList=(Nd*(Nd-1))/2;
+     duration=sizeTabuList/8;
+     // tabu list
+     moIndexedVectorTabuList<Neighbor> tl(sizeTabuList,duration);
+
+  /* =========================================================
+   *
+   * Memories
+   *
+   * ========================================================= */
+
+     moDummyIntensification<Neighbor> inten;
+     moDummyDiversification<Neighbor> div;
+     moBestImprAspiration<Neighbor> asp;
+   
+  /* =========================================================
+   *
+   * The Tabu search algorithm
+   *
+   * ========================================================= */
+
+  moTS<Neighbor> tabuSearch(neighborhood, eval, cueval, comparator, solComparator, continuator, tl, inten, div, asp);  
+ 
+  /* =========================================================
+   *
+   * Execute the local search from random sollution
+   *
+   * ========================================================= */
+ 
+  //Can be eval here, else it will be done at the beginning of the localSearch
+  eval(sol);
+
+  std::cout << "initial: " << sol<< std::endl;
+  // Create timer for timing CUDA calculation
+  cudaFuncSetCacheConfig(kernelPermutation<solution,eoMinimizingFitness, Neighbor , PPPIncrEval<Neighbor> >, cudaFuncCachePreferL1);
+  moCudaTimer timer;
+  timer.start();
+  tabuSearch(sol);
+  std::cout << "final:   " << sol << std::endl;
+  timer.stop();
+  printf("CUDA execution time = %f ms\n",timer.getTime());
+  timer.deleteTimer();
+ 
+
+  _data.cudaObject.free(dev_a);
+  _data.cudaObject.free(dev_h);
+
+  return 0;
+}
