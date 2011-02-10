50ee2d73ac63e688782185d3e2e6fc90bd6d9ca3
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-02-10 13:53:20 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2147 331e1502-861f-0410-8da2-ba01fb791d7f

--- ParadisEO-GPU/src/neighborhood/moCudaOrderNeighborhood.h
+++ ParadisEO-GPU/src/neighborhood/moCudaOrderNeighborhood.h
@@ -1,36 +1,36 @@
 /*
- <moCudaOrderNeighborhood.h>
- Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2010
+  <moCudaOrderNeighborhood.h>
+  Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2010
 
- Jerémie Humeau, Thé Van LUONG, Karima Boufaras
+  Jerémie Humeau, Thé Van LUONG, Karima Boufaras
 
- This software is governed by the CeCILL license under French law and
- abiding by the rules of distribution of free software.  You can  use,
- modify and/ or redistribute the software under the terms of the CeCILL
- license as circulated by CEA, CNRS and INRIA at the following URL
- "http://www.cecill.info".
+  This software is governed by the CeCILL license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
 
- As a counterpart to the access to the source code and  rights to copy,
- modify and redistribute granted by the license, users are provided only
- with a limited warranty  and the software's author,  the holder of the
- economic rights,  and the successive licensors  have only  limited liability.
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited liability.
 
- In this respect, the user's attention is drawn to the risks associated
- with loading,  using,  modifying and/or developing or reproducing the
- software by the user in light of its specific status of free software,
- that may mean  that it is complicated to manipulate,  and  that  also
- therefore means  that it is reserved for developers  and  experienced
- professionals having in-depth computer knowledge. Users are therefore
- encouraged to load and test the software's suitability as regards their
- requirements in conditions enabling the security of their systems and/or
- data to be ensured and,  more generally, to use and operate it in the
- same conditions as regards security.
- The fact that you are presently reading this means that you have had
- knowledge of the CeCILL license and that you accept its terms.
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
 
- ParadisEO WebSite : http://paradiseo.gforge.inria.fr
- Contact: paradiseo-help@lists.gforge.inria.fr
- */
+  ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+  Contact: paradiseo-help@lists.gforge.inria.fr
+*/
 
 #ifndef _moCudaOrderNeighborhood_h
 #define _moCudaOrderNeighborhood_h
@@ -45,52 +45,52 @@
 template<class N>
 class moCudaOrderNeighborhood: public moOrderNeighborhood<N> {
 
-public:
+ public:
 
-	/**
-	 * Define type of a solution corresponding to Neighbor
-	 */
+  /**
+   * Define type of a solution corresponding to Neighbor
+   */
 
-	typedef N Neighbor;
-	typedef typename Neighbor::EOT EOT;
+  typedef N Neighbor;
+  typedef typename Neighbor::EOT EOT;
 
-	using moOrderNeighborhood<Neighbor>::neighborhoodSize;
-	using moOrderNeighborhood<Neighbor>::currentIndex;
+  using moOrderNeighborhood<Neighbor>::neighborhoodSize;
+  using moOrderNeighborhood<Neighbor>::currentIndex;
 
-	/**
-	 * Constructor
-	 * @param _neighborhoodSize the size of the neighborhood
-	 * @param _eval show how to evaluate neighborhood of a solution at one time
-	 */
+  /**
+   * Constructor
+   * @param _neighborhoodSize the size of the neighborhood
+   * @param _eval show how to evaluate neighborhood of a solution at one time
+   */
 
-	moCudaOrderNeighborhood(unsigned int _neighborhoodSize,
-			moCudaEval<Neighbor>& _eval) :
-		moOrderNeighborhood<Neighbor> (_neighborhoodSize), eval(_eval) {
-	}
+ moCudaOrderNeighborhood(unsigned int _neighborhoodSize,
+			 moCudaEval<Neighbor>& _eval) :
+  moOrderNeighborhood<Neighbor> (_neighborhoodSize), eval(_eval) {
+  }
 
-	/**
-	 * Initialization of the neighborhood
-	 *@param _solution the solution to explore
-	 *@param _neighbor the first neighbor
-	 */
+  /**
+   * Initialization of the neighborhood
+   *@param _solution the solution to explore
+   *@param _neighbor the first neighbor
+   */
 
-	virtual void init(EOT & _solution, Neighbor & _neighbor) {
+  virtual void init(EOT & _solution, Neighbor & _neighbor) {
 
-		moOrderNeighborhood<Neighbor>::init(_solution, _neighbor);
-		//Compute all neighbors fitness at one time
-		eval.neighborhoodEval(_solution);
-	}
+    moOrderNeighborhood<Neighbor>::init(_solution, _neighbor);
+    //Compute all neighbors fitness at one time
+    eval.neighborhoodEval(_solution);
+  }
 
-	/**
-	 * Return the class name.
-	 * @return the class name as a std::string
-	 */
-	virtual std::string className() const {
-		return "moCudaOrderNeighborhood";
-	}
+  /**
+   * Return the class name.
+   * @return the class name as a std::string
+   */
+  virtual std::string className() const {
+    return "moCudaOrderNeighborhood";
+  }
 
-protected:
-	moCudaEval<Neighbor>& eval;
+ protected:
+  moCudaEval<Neighbor>& eval;
 };
 
 #endif
