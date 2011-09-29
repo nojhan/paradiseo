2531b1d2db7ac58d61bf32f711d9df75e5bd5a81
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-09-29 15:31:22 +0000

    updated
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2464 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/ParadisEO-GPU/tutorial/PPP_GPU/testSimpleTS_PPP.cu
+++ branches/ParadisEO-GPU/tutorial/PPP_GPU/testSimpleTS_PPP.cu
@@ -101,19 +101,9 @@ int main(int argc, char **argv)
     unsigned seed = seedParam.value();
 
     // Iteration number
-    eoValueParam<unsigned int> nbIterationParam(1, "nbIteration", "TS Iteration number", 'I');
+    eoValueParam<unsigned int> nbIterationParam(100, "nbIteration", "TS Iteration number", 'I');
     parser.processParam( nbIterationParam, "TS Iteration number" );
     unsigned nbIteration = nbIterationParam.value();
-
-    // size tabu list
-    eoValueParam<unsigned int> sizeTabuListParam(7, "sizeTabuList", "size of the tabu list", 'T');
-    parser.processParam( sizeTabuListParam, "Search Parameters" );
-    unsigned sizeTabuList = sizeTabuListParam.value();
-
-    // duration tabu list
-    eoValueParam<unsigned int> durationParam(7, "duration", "duration of the tabu list", 'D');
-    parser.processParam( durationParam, "Search Parameters" );
-    unsigned duration = durationParam.value();
 
     // the name of the "status" file where all actual parameter values will be saved
     string str_status = parser.ProgramName() + ".status"; // default value
@@ -204,8 +194,8 @@ int main(int argc, char **argv)
    *
    * ========================================================= */
 
-     sizeTabuList=sizeMap;
-     duration=sizeTabuList/8;
+     unsigned sizeTabuList=sizeMap;
+     unsigned duration=sizeTabuList/8;
      // tabu list
      moIndexedVectorTabuList<Neighbor> tl(sizeTabuList,duration);
 
