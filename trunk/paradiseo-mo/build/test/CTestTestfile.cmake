3ffc8253fccbfeb588ec28c47c979c09c2e1fa69
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-12-16 18:06:55 +0000

    clear build from sources
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2578 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-mo/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/test
-# Build directory: /home/me/Programmation/ProjetEclipse/paradiseo-trunk/paradiseo-mo/build/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
