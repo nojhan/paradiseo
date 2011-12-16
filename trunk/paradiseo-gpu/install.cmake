f6ab0f7f32d45c358fb8610a101a264043abea2f
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2011-12-16 17:45:28 +0000

    update cmake for independant package ParadisEO-GPu
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2569 331e1502-861f-0410-8da2-ba01fb791d7f

--- trunk/paradiseo-gpu/install.cmake
+++ trunk/paradiseo-gpu/install.cmake
@@ -11,13 +11,6 @@ SET(MO_BIN_DIR "${CMAKE_BINARY_DIR}/../../paradiseo-mo/build" CACHE PATH "Paradi
 SET(GPU_SRC_DIR "${CMAKE_SOURCE_DIR}/../paradiseo-gpu" CACHE PATH "ParadisEO-GPU source directory" FORCE)
 SET(GPU_BIN_DIR "${CMAKE_BINARY_DIR}/../../paradiseo-gpu/build" CACHE PATH "ParadisEO-GPU binary directory" FORCE)
 
-
-SET(OLDMO_SRC_DIR "${CMAKE_SOURCE_DIR}/../paradiseo-old-mo" CACHE PATH "ParadisMO-MO source directory" FORCE)
-SET(OLDMO_BIN_DIR "${CMAKE_BINARY_DIR}/../../paradiseo-old-mo/build" CACHE PATH "ParadisMO-MO binary directory" FORCE)
-
-SET(MOEO_SRC_DIR "${CMAKE_SOURCE_DIR}/../paradiseo-moeo" CACHE PATH "ParadisMOEO-MOEO source directory" FORCE)
-SET(MOEO_BIN_DIR "${CMAKE_BINARY_DIR}/../../paradiseo-moeo/build" CACHE PATH "ParadisMOEO-MOEO binary directory" FORCE)
-
 SET(PROBLEMS_SRC_DIR "${CMAKE_SOURCE_DIR}/../problems" CACHE PATH "Problems dependant source directory" FORCE)
     
 SET(CUDA_DIR "/usr/local/cuda" CACHE PATH "cuda directory" FORCE)
