afe283d30109fa0987b3b409eac7b1d4ed86dae8
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2012-01-24 01:30:08 +0000

    Update for new version
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2665 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/ParadisEO-GPU/paradiseo-gpu/src/neighborhood/moGPUBitNeighbor.h
+++ branches/ParadisEO-GPU/paradiseo-gpu/src/neighborhood/moGPUBitNeighbor.h
@@ -1,8 +1,8 @@
 /*
  <moGPUBitNeighbor.h>
- Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2010
+ Copyright (C) DOLPHIN Project-Team, INRIA Lille - Nord Europe, 2006-2012
 
- Jerémie Humeau, Boufaras Karima, Thé Van LUONG
+ Karima Boufaras, Thé Van LUONG
 
  This software is governed by the CeCILL license under French law and
  abiding by the rules of distribution of free software.  You can  use,
