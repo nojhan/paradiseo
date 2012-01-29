47fcb9be6fcfd656f4bc094e76381d02c843fa46
boufaras <boufaras@331e1502-861f-0410-8da2-ba01fb791d7f>
2012-01-29 22:32:41 +0000

    updated
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/paradiseo@2686 331e1502-861f-0410-8da2-ba01fb791d7f

--- branches/ParadisEO-GPU/paradiseo-gpu/doc/doxyfile.cmake
+++ branches/ParadisEO-GPU/paradiseo-gpu/doc/doxyfile.cmake
@@ -231,8 +231,9 @@ SKIP_FUNCTION_MACROS   = YES
 #---------------------------------------------------------------------------
 # Configuration::additions related to external references   
 #---------------------------------------------------------------------------
-TAGFILES               = @PARADISEO_EO_BIN_DIR@/doc/eo.doxytag=http://eodev.sourceforge.net/eo/doc/html \
-			 @PARADISEO_MO_BIN_DIR@/doc/mo.doxytag=http://paradiseo.gforge.inria.fr/addon/paradiseo-mo/doc \
+TAGFILES               = @EO_BIN_DIR@/doc/eo.doxytag=http://eodev.sourceforge.net/eo/doc/html \
+                         @MO_BIN_DIR@/doc/mo.doxytag=http://paradiseo.gforge.inria.fr/addon/paradiseo-mo/doc 
+
 
 GENERATE_TAGFILE       = @CMAKE_BINARY_DIR@/doc/mogpu.doxytag
 ALLEXTERNALS           = NO
