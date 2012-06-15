2851cad471681833eb0d8ca392fe3e2fa5b9d4be
Benjamin Bouvier <bnjbouv@gmail.com>
2012-06-15 17:07:29 +0200

    Configure eo-conf.cmake to enable or disable MPI use.

--- eo/eo-conf.cmake
+++ eo/eo-conf.cmake
@@ -5,3 +5,10 @@ SET(PROJECT_VERSION_MINOR 3)
 SET(PROJECT_VERSION_PATCH 0)
 SET(PROJECT_VERSION_MISC "-edge")
 
+# If you plan to use MPI, precise here where are the static libraries from
+# openmpi and boost::mpi.
+
+SET(WITH_MPI FALSE CACHE BOOL "Use mpi ?" FORCE)
+SET(MPI_DIR "put root directory of openmpi here" CACHE PATH "OpenMPI directory" FORCE)
+SET(BOOST_DIR "put root directory of boost here" CACHE PATH "Boost directory" FORCE)
+
