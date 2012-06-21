3c8e902155017f66366f3f58874521ab0ff9919b
Benjamin Bouvier <bnjbouv@gmail.com>
2012-06-21 17:23:25 +0200

    Removing useless comments in parallelApply.

--- eo/test/mpi/parallelApply.cpp
+++ eo/test/mpi/parallelApply.cpp
@@ -44,29 +44,6 @@ int main(int argc, char** argv)
         }
         cout << endl;
     }
-    /*
-    cout << "Création de l'instance..." << endl;
-    MpiNode* instance = MpiNodeStore::instance();
-    if( dynamic_cast<MasterNode*>( instance ) != 0 )
-    {
-        cout << "[Master] Algorithme d'assignation" << endl;
-        static_cast<MasterNode*>( instance )->setAssignmentAlgorithm( &algo );
-        cout << "[Master] Lancement." << endl;
-        static_cast<MasterNode*>( instance )->run( job );
-
-        for (int i = 0; i < v.size(); ++i )
-        {
-            cout << v[i] << endl;
-        }
-    } else if ( dynamic_cast<WorkerNode*>( instance ) != 0 )
-    {
-        cout << "[Worker] Lancement." << endl;
-        static_cast<WorkerNode*>( instance )->run( job );
-    } else
-    {
-        cout << "Nothing to be done;" << endl;
-    }
-    */
 
     return 0;
 }
