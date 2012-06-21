d0a00a5216a1028c71d4c1f3b4e71d9733177823
Benjamin Bouvier <bnjbouv@gmail.com>
2012-06-21 15:05:32 +0200

    Don't force user to compile with OpenMP.

--- eo/eo-conf.cmake
+++ eo/eo-conf.cmake
@@ -5,10 +5,13 @@ SET(PROJECT_VERSION_MINOR 3)
 SET(PROJECT_VERSION_PATCH 0)
 SET(PROJECT_VERSION_MISC "-edge")
 
+# If you plan to use OpenMP, put the following boolean to true :
+SET(WITH_OMP FALSE CACHE BOOL "Use OpenMP ?" FORCE)
+
 # If you plan to use MPI, precise here where are the static libraries from
 # openmpi and boost::mpi.
 
 SET(WITH_MPI FALSE CACHE BOOL "Use mpi ?" FORCE)
-SET(MPI_DIR "put root directory of openmpi here" CACHE PATH "OpenMPI directory" FORCE)
-SET(BOOST_DIR "put root directory of boost here" CACHE PATH "Boost directory" FORCE)
+SET(MPI_DIR "/mpi/directory" CACHE PATH "OpenMPI directory" FORCE)
+SET(BOOST_DIR "/boost/directory" CACHE PATH "Boost directory" FORCE)
 
