5a8ec3c3bb04e5fee621b865ce333daa5b6e7281
Johann Dreo <johann.dreo@thalesgroup.com>
2012-09-24 14:33:09 +0200

    more tests on variance computation

--- edo/test/t-variance.cpp
+++ edo/test/t-variance.cpp
@@ -6,10 +6,24 @@
 #include <es.h>
 #include <edo>
 
+typedef eoReal<eoMinimizingFitness> Vec;
+
+void check( Vec ex_mean, Vec ex_var, Vec es_mean, Vec es_var )
+{
+    std::cout << "expected  mean=" << ex_mean << " variance=" << ex_var << std::endl;
+    std::cout << "estimated mean=" << es_mean << " variance=" << es_var << std::endl;
+
+    for( unsigned int i=0; i<ex_mean.size(); ++i ) {
+        assert( es_mean[i] == ex_mean[i] );
+        assert(  es_var[i] == ex_var[i] );
+    }
+}
+
+
 int main()
 {
-    typedef eoReal<eoMinimizingFitness> Vec;
 
+    std::clog << "Variance computation on a simple vector" << std::endl;
     eoPop<Vec> pop;
     for( unsigned int i=1; i<7; ++i) {
         Vec indiv(1,i);
@@ -27,12 +41,43 @@ int main()
     Vec es_mean = distrib.mean();
     Vec es_var = distrib.variance();
 
-    std::cout << "expected  mean=" << ex_mean << " variance=" << ex_var << std::endl;
-    std::cout << "estimated mean=" << es_mean << " variance=" << es_var << std::endl;
+    check( ex_mean, ex_var, es_mean, es_var );
 
-    for( unsigned int i=0; i<ex_mean.size(); ++i ) {
-        assert( es_mean[i] == ex_mean[i] );
-        assert(  es_var[i] == ex_var[i] );
-    }
+
+    // Test with negative values
+    std::clog << "Variance computation on negative values" << std::endl;
+    pop.clear();
+
+    pop.push_back( Vec(1, 11.0) );
+    pop.push_back( Vec(1, -4.0) );
+    pop.push_back( Vec(1, 11.0) );
+    pop.push_back( Vec(1, 14.0) );
+    pop.push_back( Vec(1, -2.0) );
+
+    std::clog << pop << std::endl;
+
+    distrib = estimator(pop);
+
+    ex_mean = Vec(1,6.0);
+    ex_var = Vec(1,278.0/5);
+    es_mean = distrib.mean();
+    es_var = distrib.variance();
+
+    check( ex_mean, ex_var, es_mean, es_var );
+
+
+    // test single individual
+    std::clog << "Variance computation on a pop with a single individual" << std::endl;
+    pop.clear();
+    pop.push_back( Vec(1, 0.0) );
+
+    distrib = estimator(pop);
+
+    ex_mean = Vec(1,0.0);
+    ex_var = Vec(1,0.0);
+    es_mean = distrib.mean();
+    es_var = distrib.variance();
+
+    check( ex_mean, ex_var, es_mean, es_var );
 }
 
