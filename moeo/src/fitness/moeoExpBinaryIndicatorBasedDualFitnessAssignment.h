b132f48de2ed7f36570dbb1bdd9fbf26f4493984
Johann Dreo <johann.dreo@thalesgroup.com>
2013-06-13 10:37:25 +0200

    Insert a copyright header

--- moeo/src/fitness/moeoExpBinaryIndicatorBasedDualFitnessAssignment.h
+++ moeo/src/fitness/moeoExpBinaryIndicatorBasedDualFitnessAssignment.h
@@ -1,3 +1,28 @@
+/*
+
+(c) 2013 Thales group
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; version 2
+    of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Contact: http://eodev.sourceforge.net
+
+Authors:
+    Johann Dréo <johann.dreo@thalesgroup.com>
+
+*/
+
 
 #ifndef MOEOEXPBINARYINDICATORBASEDDUALFITNESSASSIGNMENT_H_
 #define MOEOEXPBINARYINDICATORBASEDDUALFITNESSASSIGNMENT_H_
