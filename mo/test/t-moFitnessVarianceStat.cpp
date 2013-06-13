b1067a05259354492bd9b58e824f4f974da2f32d
LPTK <lionel.parreaux@gmail.com>
2013-06-13 14:55:01 +0200

    minor fixes

--- mo/test/t-moFitnessVarianceStat.cpp
+++ mo/test/t-moFitnessVarianceStat.cpp
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 
 #include <eo>
-//#include "eoReal.h"
+#include "es/eoReal.h"
 #include "continuator/moFitnessVarianceStat.h"
 #include "neighborhood/moRealNeighbor.h"
 #include "neighborhood/moRealNeighborhood.h"
@@ -16,9 +16,9 @@ typedef moRealNeighbor< EOT > Neighbor;
 int main(int ac, char** av)
 {
     //moNeighborhoodStat<Neighbor> nhStat
-    moFitnessVarianceStat<Neighbor> stat();
+    moFitnessVarianceStat<EOT> stat;
     EOT solution(2, 5);
     stat(solution);
     //assert(stat.value() == 1);
-    std::cout << "ok " << stat.value() << endl;
+    std::cout << "ok " << stat.value() << std::endl;
 }
