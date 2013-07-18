6548a0e223b1cf78d51cef948342a60e86e0242e
LPTK <lionel.parreaux@gmail.com>
2013-07-18 14:13:35 +0200

    Added a field for saving the fitness of the solution (useful for monitoring)

--- mo/src/algo/moLocalSearch.h
+++ mo/src/algo/moLocalSearch.h
@@ -47,99 +47,116 @@
 template<class Neighbor>
 class moLocalSearch: public eoMonOp<typename Neighbor::EOT> {
 public:
-	typedef moNeighborhood<Neighbor> Neighborhood;
-	typedef moNeighborhoodExplorer<Neighbor> NeighborhoodExplorer;
-	typedef typename Neighbor::EOT EOT;
+    typedef moNeighborhood<Neighbor> Neighborhood;
+    typedef moNeighborhoodExplorer<Neighbor> NeighborhoodExplorer;
+    typedef typename Neighbor::EOT EOT;
+    
+    /**
+     * Constructor of a moLocalSearch
+     * @param _searchExpl a neighborhood explorer
+     * @param _cont an external continuator (can be a checkpoint!)
+     * @param _fullEval a full evaluation function
+     */
+    moLocalSearch(NeighborhoodExplorer& _searchExpl, moContinuator<Neighbor> & _cont, eoEvalFunc<EOT>& _fullEval)
+    : searchExplorer(_searchExpl), cont(&_cont), fullEval(_fullEval), currentSolutionFitness(0)
+    { }
+    
+    /**
+     * Run the local search on a solution
+     * @param _solution the related solution
+     */
+    virtual bool operator()(EOT & _solution) {
 
-	/**
-	 * Constructor of a moLocalSearch
-	 * @param _searchExpl a neighborhood explorer
-	 * @param _cont an external continuator (can be a checkpoint!)
-	 * @param _fullEval a full evaluation function
-	 */
-	moLocalSearch(NeighborhoodExplorer& _searchExpl,
-			moContinuator<Neighbor> & _cont, eoEvalFunc<EOT>& _fullEval) :
-		searchExplorer(_searchExpl), cont(&_cont), fullEval(_fullEval) {
-	}
-	;
+        if (_solution.invalid())
+            fullEval(_solution);
+        
+        // initialization of the parameter of the search (for example fill empty the tabu list)
+        searchExplorer.initParam(_solution);
+        
+        // initialization of the external continuator (for example the time, or the number of generations)
+        cont->init(_solution);
+        
+        bool b;
+        
+        do
+        {
+            currentSolutionFitness = _solution.fitness();
+            
+            // explore the neighborhood of the solution
+            searchExplorer(_solution);
+            // if a solution in the neighborhood can be accepted
+            if (searchExplorer.accept(_solution)) {
+                searchExplorer.move(_solution);
+                searchExplorer.moveApplied(true);
+            } else
+                searchExplorer.moveApplied(false);
 
-	/**
-	 * Run the local search on a solution
-	 * @param _solution the related solution
-	 */
-	virtual bool operator()(EOT & _solution) {
+            // update the parameter of the search (for ex. Temperature of the SA)
+            searchExplorer.updateParam(_solution);
 
-		if (_solution.invalid())
-			fullEval(_solution);
+            b = (*cont)(_solution); 
+        }
+        while (b && searchExplorer.isContinue(_solution));
 
-		// initialization of the parameter of the search (for example fill empty the tabu list)
-		searchExplorer.initParam(_solution);
+        searchExplorer.terminate(_solution);
 
-		// initialization of the external continuator (for example the time, or the number of generations)
-		cont->init(_solution);
+        cont->lastCall(_solution);
 
-		bool b;
-		do {
-			// explore the neighborhood of the solution
-			searchExplorer(_solution);
-			// if a solution in the neighborhood can be accepted
-			if (searchExplorer.accept(_solution)) {
-				searchExplorer.move(_solution);
-				searchExplorer.moveApplied(true);
-			} else
-				searchExplorer.moveApplied(false);
+        return true;
+    }
 
-			// update the parameter of the search (for ex. Temperature of the SA)
-			searchExplorer.updateParam(_solution);
+    /**
+     * Set an external continuator
+     * @param _cont the external continuator
+     */
+    void setContinuator(moContinuator<Neighbor> & _cont) {
+        cont = &_cont;
+    }
 
-			b = (*cont)(_solution);
-		} while (b && searchExplorer.isContinue(_solution));
+    /**
+     * external continuator object
+     *
+     * @overload
+     * @return the external continuator
+     */
+    moContinuator<Neighbor>* getContinuator() const {
+        return cont;
+    }
 
-		searchExplorer.terminate(_solution);
+    /**
+     * to get the neighborhood explorer
+     *
+     * @overload
+     * @return the neighborhood explorer
+     */
+    moNeighborhoodExplorer<Neighbor> & getNeighborhoodExplorer() const {
+        return searchExplorer;
+    }
 
-		cont->lastCall(_solution);
-
-		return true;
-	}
-	;
-
-	/**
-	 * Set an external continuator
-	 * @param _cont the external continuator
-	 */
-	void setContinuator(moContinuator<Neighbor> & _cont) {
-		cont = &_cont;
-	}
-
-	/**
-	 * external continuator object
-	 *
-	 * @overload
-	 * @return the external continuator
-	 */
-	moContinuator<Neighbor>* getContinuator() const {
-		return cont;
-	}
-
-	/**
-	 * to get the neighborhood explorer
-	 *
-	 * @overload
-	 * @return the neighborhood explorer
-	 */
-	moNeighborhoodExplorer<Neighbor> & getNeighborhoodExplorer() const {
-		return searchExplorer;
-	}
+    /**
+     * Useful for monitoring with anb eoGetterUpdater
+     * @return the fitness of the last computed solution
+     */
+    double getCurrentSolutionFitness() const {
+        return currentSolutionFitness;
+    }
 
 protected:
-	// make the exploration of the neighborhood according to a local search heuristic
-	moNeighborhoodExplorer<Neighbor>& searchExplorer;
+    
+    // make the exploration of the neighborhood according to a local search heuristic
+    moNeighborhoodExplorer<Neighbor>& searchExplorer;
 
-	// external continuator
-	moContinuator<Neighbor> * cont;
+    // external continuator
+    moContinuator<Neighbor> * cont;
 
-	//full evaluation function
-	eoEvalFunc<EOT>& fullEval;
+    //full evaluation function
+    eoEvalFunc<EOT>& fullEval;
+    
+private:
+    
+    // fitness of the last computed solution
+    double currentSolutionFitness;
+    
 };
 
 #endif
