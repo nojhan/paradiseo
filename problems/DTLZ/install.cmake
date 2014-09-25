749119070c66a0c8937648356cc8c554b0385d50
manu <manuel.lopez-ibanez@ulb.ac.be>
2014-09-25 22:33:26 +0200

    * Fixes in the build system to be able to build DTLZ from within the GIT repository. It seems the paths have changed since this library was developed.

--- problems/DTLZ/install.cmake
+++ problems/DTLZ/install.cmake
@@ -1,28 +1,28 @@
-#########################################################################################################
+###############################################################################
 # 1) ParadisEO install: SIMPLE Configuration
-#########################################################################################################
+###############################################################################
 
-#  Here, just specify PARADISEO_DIR : the directory where ParadisEO has been installed
-SET(PARADISEO_DIR "/home/humeau/Bureau/paradiseo-1.3-beta2/" CACHE PATH "ParadisEO directory" FORCE)
 
-#########################################################################################################
+# Here, just specify PARADISEO_DIR : the directory where ParadisEO has
+# been installed
+SET(PARADISEO_DIR "${CMAKE_BINARY_DIR}/../../../" CACHE PATH "ParadisEO directory" FORCE)
+
+###############################################################################
  
  
  
-#########################################################################################################
+###############################################################################
 # 2) ParadisEO install: ADVANCED Configuration
-#########################################################################################################
+###############################################################################
 
-SET(PARADISEO_EO_SRC_DIR "${PARADISEO_DIR}/paradiseo-eo" CACHE PATH "ParadisEO-EO source directory" FORCE)
-SET(PARADISEO_EO_BIN_DIR "${PARADISEO_DIR}/paradiseo-eo/build" CACHE PATH "ParadisEO-EO binary directory" FORCE)
+SET(PARADISEO_EO_SRC_DIR "${PARADISEO_DIR}/eo" CACHE PATH "ParadisEO-EO source directory" FORCE)
+SET(PARADISEO_EO_BIN_DIR "${PARADISEO_DIR}/build/eo" CACHE PATH "ParadisEO-EO binary directory" FORCE)
 
-SET(PARADISEO_MO_SRC_DIR "${PARADISEO_DIR}/paradiseo-mo" CACHE PATH "ParadisEO-MO source directory" FORCE)
-SET(PARADISEO_MO_BIN_DIR "${PARADISEO_DIR}/paradiseo-mo/build" CACHE PATH "ParadisEO-MO binary directory" FORCE)
+SET(PARADISEO_MO_SRC_DIR "${PARADISEO_DIR}/mo" CACHE PATH "ParadisEO-MO source directory" FORCE)
+SET(PARADISEO_MO_BIN_DIR "${PARADISEO_DIR}/build/mo" CACHE PATH "ParadisEO-MO binary directory" FORCE)
 
-SET(PARADISEO_MOEO_SRC_DIR "${PARADISEO_DIR}/paradiseo-moeo" CACHE PATH "ParadisEO-MOEO source directory" FORCE)
-SET(PARADISEO_MOEO_BIN_DIR "${PARADISEO_DIR}/paradiseo-moeo/build" CACHE PATH "ParadisEO-MOEO binary directory" FORCE)
+SET(PARADISEO_MOEO_SRC_DIR "${PARADISEO_DIR}/moeo" CACHE PATH "ParadisEO-MOEO source directory" FORCE)
+SET(PARADISEO_MOEO_BIN_DIR "${PARADISEO_DIR}/build/moeo" CACHE PATH "ParadisEO-MOEO binary directory" FORCE)
 
-SET(PARADISEO_PEO_SRC_DIR "${PARADISEO_DIR}/paradiseo-peo" CACHE PATH "ParadisEO-PEO source directory" FORCE)
-SET(PARADISEO_PEO_BIN_DIR "${PARADISEO_DIR}/paradiseo-peo/build" CACHE PATH "ParadisEO-PEO binary directory" FORCE)
-#########################################################################################################
+###############################################################################
 
