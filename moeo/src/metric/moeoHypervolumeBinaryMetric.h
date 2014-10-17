5a74f9a3c67c2dfc6fafe0d9499154de437ba028
Arnaud Liefooghe <arnaud.liefooghe@univ-lille1.fr>
2014-10-17 15:27:43 +0200

    trick to allow maximization

--- moeo/src/metric/moeoHypervolumeBinaryMetric.h
+++ moeo/src/metric/moeoHypervolumeBinaryMetric.h
@@ -1,39 +1,39 @@
 /*
-* <moeoHypervolumeBinaryMetric.h>
-* Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
-* (C) OPAC Team, LIFL, 2002-2007
-*
-* July 2013: Bug fix in the recursive call of hypervolume (corrected thanks to Yann Semet and Dimo Brockhoff)
-*
-*
-* This software is governed by the CeCILL license under French law and
-* abiding by the rules of distribution of free software.  You can  use,
-* modify and/ or redistribute the software under the terms of the CeCILL
-* license as circulated by CEA, CNRS and INRIA at the following URL
-* "http://www.cecill.info".
-*
-* As a counterpart to the access to the source code and  rights to copy,
-* modify and redistribute granted by the license, users are provided only
-* with a limited warranty  and the software's author,  the holder of the
-* economic rights,  and the successive licensors  have only  limited liability.
-*
-* In this respect, the user's attention is drawn to the risks associated
-* with loading,  using,  modifying and/or developing or reproducing the
-* software by the user in light of its specific status of free software,
-* that may mean  that it is complicated to manipulate,  and  that  also
-* therefore means  that it is reserved for developers  and  experienced
-* professionals having in-depth computer knowledge. Users are therefore
-* encouraged to load and test the software's suitability as regards their
-* requirements in conditions enabling the security of their systems and/or
-* data to be ensured and,  more generally, to use and operate it in the
-* same conditions as regards security.
-* The fact that you are presently reading this means that you have had
-* knowledge of the CeCILL license and that you accept its terms.
-*
-* ParadisEO WebSite : http://paradiseo.gforge.inria.fr
-* Contact: paradiseo-help@lists.gforge.inria.fr
-*
-*/
+ * <moeoHypervolumeBinaryMetric.h>
+ * Copyright (C) DOLPHIN Project-Team, INRIA Futurs, 2006-2007
+ * (C) OPAC Team, LIFL, 2002-2007
+ *
+ * July 2013: Bug fix in the recursive call of hypervolume (corrected thanks to Yann Semet and Dimo Brockhoff)
+ *
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ * ParadisEO WebSite : http://paradiseo.gforge.inria.fr
+ * Contact: paradiseo-help@lists.gforge.inria.fr
+ *
+ */
 //-----------------------------------------------------------------------------
 
 #ifndef MOEOHYPERVOLUMEBINARYMETRIC_H_
@@ -57,32 +57,24 @@
  */
 template < class ObjectiveVector >
 class moeoHypervolumeBinaryMetric : public moeoNormalizedSolutionVsSolutionBinaryMetric < ObjectiveVector, double >
-  {
-  public:
-
+{
+public:
+    
     /**
      * Ctor
      * @param _rho value used to compute the reference point from the worst values for each objective (default : 1.1)
      */
     moeoHypervolumeBinaryMetric(double _rho = 1.1) : moeoNormalizedSolutionVsSolutionBinaryMetric<ObjectiveVector, double>(), rho(_rho)
     {
-      // not-a-maximization problem check
-      for (unsigned int i=0; i<ObjectiveVector::Traits::nObjectives(); i++)
-        {
-          if (ObjectiveVector::Traits::maximizing(i))
-            {
-              throw std::runtime_error("Hypervolume binary metric not yet implemented for a maximization problem in moeoHypervolumeBinaryMetric");
-            }
-        }
-      // consistency check
-      if (rho < 1)
+        // consistency check
+        if (rho < 1)
         {
             eo::log << eo::warnings << "Warning, value used to compute the reference point rho for the hypervolume calculation must not be smaller than 1, adjusted to 1" << std::endl;
-          rho = 1;
+            rho = 1;
         }
     }
-
-
+    
+    
     /**
      * Returns the volume of the space that is dominated by _o2 but not by _o1 with respect to a reference point computed using rho.
      * @warning don't forget to set the bounds for every objective before the call of this function
@@ -91,30 +83,41 @@ class moeoHypervolumeBinaryMetric : public moeoNormalizedSolutionVsSolutionBinar
      */
     double operator()(const ObjectiveVector & _o1, const ObjectiveVector & _o2)
     {
-      double result;
-      // if _o2 is dominated by _o1
-      if ( paretoComparator(_o2,_o1) )
+        double result;
+        // transform maximizing objectives into minimizing objectives
+        ObjectiveVector o1 = _o1;
+        ObjectiveVector o2 = _o2;
+        for (unsigned int i=0; i<ObjectiveVector::Traits::nObjectives(); i++)
         {
-          result = - hypervolume(_o1, _o2, ObjectiveVector::Traits::nObjectives()-1);
+            if (ObjectiveVector::Traits::maximizing(i))
+            {
+                o1[i] = bounds[i].maximum() - o1[i] + bounds[i].minimum();
+                o2[i] = bounds[i].maximum() - o2[i] + bounds[i].minimum();
+            }
         }
-      else
+        // if _o2 is dominated by _o1
+        if ( paretoComparator(_o2,_o1) )
         {
-          result = hypervolume(_o2, _o1, ObjectiveVector::Traits::nObjectives()-1);
+            result = - hypervolume(_o1, _o2, ObjectiveVector::Traits::nObjectives()-1);
         }
-      return result;
+        else
+        {
+            result = hypervolume(_o2, _o1, ObjectiveVector::Traits::nObjectives()-1);
+        }
+        return result;
     }
-
-
-  private:
-
+    
+    
+private:
+    
     /** value used to compute the reference point from the worst values for each objective */
     double rho;
     /** the bounds for every objective */
     using moeoNormalizedSolutionVsSolutionBinaryMetric < ObjectiveVector, double > :: bounds;
     /** Functor to compare two objective vectors according to Pareto dominance relation */
     moeoParetoObjectiveVectorComparator < ObjectiveVector > paretoComparator;
-
-
+    
+    
     /**
      * Returns the volume of the space that is dominated by _o2 but not by _o1 with respect to a reference point computed using rho for the objective _obj.
      * @param _o1 the first objective vector
@@ -124,48 +127,48 @@ class moeoHypervolumeBinaryMetric : public moeoNormalizedSolutionVsSolutionBinar
      */
     double hypervolume(const ObjectiveVector & _o1, const ObjectiveVector & _o2, const unsigned int _obj, const bool _flag = false)
     {
-      double result;
-      double range = rho * bounds[_obj].range();
-      double max = bounds[_obj].minimum() + range;
-
-      // value of _1 for the objective _obj
-      double v1 = _o1[_obj];
-      // value of _2 for the objective _obj (if _flag=true, v2=max)
-      double v2;
-      if (_flag)
+        double result;
+        double range = rho * bounds[_obj].range();
+        double max = bounds[_obj].minimum() + range;
+        
+        // value of _1 for the objective _obj
+        double v1 = _o1[_obj];
+        // value of _2 for the objective _obj (if _flag=true, v2=max)
+        double v2;
+        if (_flag)
         {
-          v2 = max;
+            v2 = max;
         }
-      else
+        else
         {
-          v2 = _o2[_obj];
+            v2 = _o2[_obj];
         }
-      // computation of the volume
-      if (_obj == 0)
+        // computation of the volume
+        if (_obj == 0)
         {
-          if (v1 < v2)
+            if (v1 < v2)
             {
-              result = (v2 - v1) / range;
+                result = (v2 - v1) / range;
             }
-          else
+            else
             {
-              result = 0;
-            }
-        }
-      else
-        {
-          if (v1 < v2)
-            {
-              result = ( hypervolume(_o1, _o2, _obj-1, true) * (v2 - v1) / range ) + ( hypervolume(_o1, _o2, _obj-1) * (max - v2) / range );
-            }
-          else
-            {
-              result = hypervolume(_o1, _o2, _obj-1) * (max - v1) / range;
+                result = 0;
             }
         }
-      return result;
+        else
+        {
+            if (v1 < v2)
+            {
+                result = ( hypervolume(_o1, _o2, _obj-1, true) * (v2 - v1) / range ) + ( hypervolume(_o1, _o2, _obj-1) * (max - v2) / range );
+            }
+            else
+            {
+                result = hypervolume(_o1, _o2, _obj-1) * (max - v1) / range;
+            }
+        }
+        return result;
     }
-
-  };
+    
+};
 
 #endif /*MOEOHYPERVOLUMEBINARYMETRIC_H_*/
