ff322ba27bd0f698fe57c1c676a043e11d7917e0
nojhan <nojhan@nojhan.net>
2019-04-19 06:52:38 +0200

    fix missing API update
    - tutorial/Lesson3 was still using the old eoStdoutMonitor interface

--- eo/tutorial/Lesson3/exercise3.1.cpp
+++ eo/tutorial/Lesson3/exercise3.1.cpp
@@ -289,7 +289,7 @@ void main_function(int argc, char **argv)
     checkpoint.add(fdcStat);
 
     // The Stdout monitor will print parameters to the screen ...
-    eoStdoutMonitor monitor(false);
+    eoStdoutMonitor monitor;
 
     // when called by the checkpoint (i.e. at every generation)
     checkpoint.add(monitor);
