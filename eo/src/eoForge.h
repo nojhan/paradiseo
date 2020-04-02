ccb6bbed7cf84109557b6af147e7ef2942aa15a3
nojhan <nojhan@nojhan.net>
2020-04-02 16:39:01 +0200

    comment out useless tuple_printer

--- eo/src/eoForge.h
+++ eo/src/eoForge.h
@@ -25,31 +25,33 @@
 
 #include <string>
 #include <tuple>
-template<typename Type, unsigned N, unsigned Last>
-struct tuple_printer {
 
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value) << ", ";
-        tuple_printer<Type, N + 1, Last>::print(out, value);
-    }
-};
-
-template<typename Type, unsigned N>
-struct tuple_printer<Type, N, N> {
-
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value);
-    }
-
-};
-
-template<typename... Types>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
-    out << "(";
-    tuple_printer<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
-    out << ")";
-    return out;
-}
+// In case you want to debug arguments captured in tuples:
+// template<typename Type, unsigned N, unsigned Last>
+// struct tuple_printer {
+//
+//     static void print(std::ostream& out, const Type& value) {
+//         out << std::get<N>(value) << ", ";
+//         tuple_printer<Type, N + 1, Last>::print(out, value);
+//     }
+// };
+//
+// template<typename Type, unsigned N>
+// struct tuple_printer<Type, N, N> {
+//
+//     static void print(std::ostream& out, const Type& value) {
+//         out << std::get<N>(value);
+//     }
+//
+// };
+//
+// template<typename... Types>
+// std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
+//     out << "(";
+//     tuple_printer<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
+//     out << ")";
+//     return out;
+// }
 
 /**
  * @defgroup Foundry Tools for automatic algorithms assembling, selection and search.
