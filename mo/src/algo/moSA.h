846006c613478e00550ef192fd76dc549c41fac1
Johann Dreo <johann.dreo@pasteur.fr>
2024-09-21 10:22:47 +0200

    feat(moSA): adds a constructor without cool but with cont

--- mo/src/algo/moSA.h
+++ mo/src/algo/moSA.h
@@ -80,6 +80,20 @@ public:
     {}
 
     /**
+     * Constructor without cooling schedule, but with a continuator.
+     *
+     * @param _neighborhood the neighborhood
+     * @param _fullEval the full evaluation function
+     * @param _eval neighbor's evaluation function
+     * @param _cont an external continuator
+     */
+    moSA(Neighborhood& _neighborhood, eoEvalFunc<EOT>& _fullEval, moEval<Neighbor>& _eval, moContinuator<Neighbor>& _cont):
+            moLocalSearch<Neighbor>(explorer, _cont, _fullEval),
+            defaultCool(0, 0, 0, 0),
+            explorer(_neighborhood, _eval, defaultSolNeighborComp, defaultCool)
+    {}
+
+    /**
      * General constructor for a simulated annealing
      * @param _neighborhood the neighborhood
      * @param _fullEval the full evaluation function
