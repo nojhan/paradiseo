b8e393bf3650b192fa95d59fa240557f00d37d4b
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-15 16:24:20 +0200

    reset: using assert for validation

--- eo/src/eoRankingCached.h
+++ eo/src/eoRankingCached.h
@@ -59,19 +59,7 @@ public:
     */
     eoRankingCached(double _p = 2.0, double _e = 1.0) : pressure(_p), exponent(_e), cached_pSize(0)
     {
-        if (pressure <= 1.0)
-        {
-            std::string msg = "eoRankingCached: pressure must be > 1.0";
-            eo::log << eo::errors << "ERROR: " << msg << std::endl;
-            throw eoException(msg);
-        }
-
-        if (exponent > 2.0)
-        {
-            std::string msg = "eoRankingCached: exponent must be <= 2.0";
-            eo::log << eo::errors << "ERROR: " << msg << std::endl;
-            throw eoException(msg);
-        }
+        assert(1 < pressure and exponent <= 2);
     }
 
     /*
